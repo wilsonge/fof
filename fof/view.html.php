50c24d32cc2dc82178eec9ef7d4d6329c7b2c896
Davide Tampellini <tampe125@gmail.com>
2012-04-15 10:00:20 +0200

    Removes dummy links from toolbar. With SEF active, the "#" will be followed before the form submission.

--- fof/view.html.php
+++ fof/view.html.php
@@ -12,7 +12,7 @@ jimport('joomla.application.component.view');
 
 /**
  * FrameworkOnFramework HTML List View class
- * 
+ *
  * FrameworkOnFramework is a set of classes which extend Joomla! 1.5 and later's
  * MVC framework with features making maintaining complex software much easier,
  * without tedious repetitive copying of the same code over and over again.
@@ -21,32 +21,32 @@ class FOFViewHtml extends FOFView
 {
 	/** @var array Data lists */
 	protected $lists = null;
-	
+
 	/** @var array Permissions map */
 	protected $perms = null;
-	
+
 	/** @var bool Set to true to render a toolbar in the front-end */
 	protected $frontendToolbar = false;
 
 	/**
 	 * Class constructor
-	 * 
+	 *
 	 * @param array $config Configuration parameters
 	 */
 	function  __construct($config = array()) {
 		parent::__construct($config);
-		
+
 		$this->config = $config;
-		
+
 		// Get the input
 		if(array_key_exists('input', $config)) {
 			$this->input = $config['input'];
 		} else {
 			$this->input = JRequest::get('default', 3);
 		}
-		
+
 		$this->lists = new JObject();
-		
+
 		if(version_compare(JVERSION, '1.6.0', 'ge')) {
 			$user = JFactory::getUser();
 			$perms = (object)array(
@@ -65,7 +65,7 @@ class FOFViewHtml extends FOFView
 		}
 		$this->assign('aclperms', $perms);
 		$this->perms = $perms;
-		
+
 		if(array_key_exists('frontendToolbar', $this->config)) {
 			$this->frontendToolbar = $this->config['frontendToolbar'];
 		}
@@ -73,9 +73,9 @@ class FOFViewHtml extends FOFView
 
 	/**
 	 * Displays the view
-	 * 
+	 *
 	 * @param string $tpl The template to use
-	 * 
+	 *
 	 * @return bool
 	 */
 	function  display($tpl = null)
@@ -91,9 +91,9 @@ class FOFViewHtml extends FOFView
 		} else {
 			$result = $this->onDisplay();
 		}
-		
+
 		if($result === false) { return; }
-		
+
 		$toolbar = FOFToolbar::getAnInstance(FOFInput::getCmd('option','com_foobar',$this->input), $this->config);
 		$toolbar->perms = $this->perms;
 		$toolbar->renderToolbar(FOFInput::getCmd('view','cpanel',$this->input), $task, $this->input);
@@ -101,7 +101,7 @@ class FOFViewHtml extends FOFView
 		//if i'm not the admin and i have some buttons or a title to show, let's render them before the layout
 		//Framework will only create the HTML structure, 3rd part developers will have to add CSS to correctly style it
 		$isAdmin = version_compare(JVERSION, '1.6.0', 'ge') ? (!JFactory::$application ? false : JFactory::getApplication()->isAdmin()) : JFactory::getApplication()->isAdmin();
-		if(!$isAdmin && $this->frontendToolbar) 
+		if(!$isAdmin && $this->frontendToolbar)
 		{
 			$title = JFactory::getApplication()->get('JComponentTitle');
 			$bar = JToolBar::getInstance('toolbar');
@@ -122,15 +122,16 @@ class FOFViewHtml extends FOFView
 
 				$html[] = '<div style="clear:both"></div>';
 				$html[] = '</div>';
-
-				echo implode("\n", $html);
+				$html = implode("\n", $html);
+				$html = str_replace('href="#"', '', $html);
+				echo $html;
 			}
 		}
 
 		// Show the view
 		parent::display($tpl);
 	}
-	
+
 	protected function onBrowse($tpl = null)
 	{
 		// When in interactive browsing mode, save the state to the session
@@ -142,7 +143,7 @@ class FOFViewHtml extends FOFView
 	{
 		$view = FOFInput::getCmd('view','cpanel',$this->input);
 		if(in_array($view,array('cpanel','cpanels'))) return;
-		
+
 		// Load the model
 		$model = $this->getModel();
 
@@ -154,7 +155,7 @@ class FOFViewHtml extends FOFView
 		$this->assign   ( 'items',		$model->getItemList() );
 		$this->assign   ( 'pagination',	$model->getPagination());
 		$this->assignRef( 'lists',		$this->lists);
-		
+
 		return true;
 	}
 
@@ -171,7 +172,7 @@ class FOFViewHtml extends FOFView
 		// An editor is an editor, no matter if the record is new or old :p
 		return $this->onAdd();
 	}
-	
+
 	protected function onRead($tpl = null)
 	{
 		// All I need is to read the record
