c630a4f5bc4237e4a42e9870a067abd75024ece8
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-11-20 11:28:52 +0200

    Refactored FOFInput as a subclass of JInput

--- fof/input.php
+++ fof/input.php
@@ -8,45 +8,133 @@
 // Protect from unauthorized access
 defined('_JEXEC') or die();
 
-jimport("joomla.environment.request");
-
 /**
- * FrameworkOnFramework input handling class
- * 
- * FrameworkOnFramework is a set of classes whcih extend Joomla! 1.5 and later's
- * MVC framework with features making maintaining complex software much easier,
- * without tedious repetitive copying of the same code over and over again.
+ * FrameworkOnFramework input handling class. Extends upon the JInput class.
  */
-class FOFInput
+class FOFInput extends JInput
 {
-	public static function getVar($name, $default = null, $input = null, $type = 'none', $mask = 0)
+	/**
+	 * Public constructor. Overriden to allow specifying the global input array
+	 * to use as a string and instantiate from an objetc holding variables.
+	 * 
+	 * @param array|string|object|null $source Source data; set null to use $_REQUEST
+	 * @param array $options Filter options
+	 */
+	public function __construct($source = null, array $options = array())
 	{
-		if(is_null($input)) {
-			$var = JRequest::getVar($name, $default, 'default', $type, $mask);
-		} elseif(is_string($input)) {
-			$var = JRequest::getVar($name, $default, $input, $type, $mask);
-		} elseif (isset($input[$name]) && $input[$name] !== null) {
-			// Get the variable from the input hash and clean it
-			$var = self::_cleanVar($input[$name], $mask, $type);
-
-			// Handle magic quotes compatability
-			if (get_magic_quotes_gpc() && ($var != $default)) {
-				$var = self::_stripSlashesRecursive( $var );
+		if(is_string($source)) {
+			$hash = strtoupper($source);
+			switch($hash) {
+				case 'GET':
+					$source = &$_GET;
+					break;
+				case 'POST':
+					$source = &$_POST;
+					break;
+				case 'FILES':
+					$source = &$_FILES;
+					break;
+				case 'COOKIE':
+					$source = &$_COOKIE;
+					break;
+				case 'ENV':
+					$source = &$_ENV;
+					break;
+				case 'SERVER':
+					$source = &$_SERVER;
+					break;
+				default:
+					$source = &$_REQUEST;
+					$hash = 'REQUEST';
+					break;
+			}
+		} elseif(is_object($source)) {
+			try {
+				$source = (array)$source;
+			} catch (Exception $exc) {
+				$source = null;
 			}
 		}
-		elseif ($default !== null) {
-			// Clean the default value
-			$var = self::_cleanVar($default, $mask, $type);
-		}
-		else {
-			$var = $default;
-		}
 		
-		return $var;
+		parent::__construct($source, $options);
 	}
 	
+	/**
+	 * Gets a value from the input data. Overriden to allow specifying a filter
+	 * mask.
+	 *
+	 * @param   string  $name     Name of the value to get.
+	 * @param   mixed   $default  Default value to return if variable does not exist.
+	 * @param   string  $filter   Filter to apply to the value.
+	 *
+	 * @return  mixed  The filtered input value.
+	 */
+	public function get($name, $default = null, $filter = 'cmd', $mask = 0)
+	{
+		if (isset($this->data[$name]))
+		{
+			return $this->_cleanVar($this->data[$name], $mask, $filter);
+		}
+
+		return $default;
+	}
+	
+	/**
+	 * Old static methods are now deprecated. This magic method makes sure there
+	 * is a continuity in our approach. The downside is that it's only compatible
+	 * with PHP 5.3.0. Sorry!
+	 * 
+	 * @param string $name Name of the method we're calling
+	 * @param array $arguments The arguments passed to the method
+	 * @return mixed
+	 */
+	public function __callStatic($name, $arguments) {
+		JLog::add('FOFInput: static getXXX() methods are deprecated. Use the input object\'s methods instead.', JLog::WARNING, 'deprecated');
+		
+		if(substr($name, 0, 3) == 'get') {
+			// Initialise arguments
+			$name = array_unshift($arguments);
+			$default = array_unshift($arguments);
+			$input = array_unshift($arguments);
+			$type = 'none';
+			$mask = 0;
+			
+			$type = strtolower(substr($name, 4));
+			if($type == 'var') {
+				$type = array_unshift($arguments);
+				$mask = array_unshift($arguments);
+			}
+			if(is_null($type)) {
+				$type = 'none';
+			}
+			if(is_null($mask)) {
+				$mask = 0;
+			}
+			
+			if(!($input instanceof FOFInput) && !($input instanceof JInput)) {
+				$input = new FOFInput($input);
+			}
+			return $input->get($name, $default, $type, $mask);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Sets an input variable. WARNING: IT SHOULD NO LONGER BE USED!
+	 * 
+	 * @param type $name
+	 * @param type $value
+	 * @param type $input
+	 * @param type $overwrite
+	 * @return type
+	 * 
+	 * @deprecated
+	 */
 	public static function setVar($name, $value = null, &$input = array(), $overwrite = true)
 	{
+		JLog::add('FOFInput::setVar() is deprecated. Use set() instead.', JLog::WARNING, 'deprecated');
+		
 		if(empty($input)) {
 			return JRequest::setVar($name, $value, 'default', $overwrite);
 		} elseif(is_string($input)) {
@@ -64,34 +152,16 @@ class FOFInput
 		}
 	}
 	
-	public static function getBool($name, $default = false, $input = array())
-	{
-		return self::getVar($name, $default, $input, 'bool');
-	}
-	
-	public static function getWord($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'word');
-	}
-	
-	public static function getCmd($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'cmd');
-	}
-	
 	public static function getString($name, $default = '', $input = array(), $mask = 0)
 	{
+		JLog::add('FOFInput::getString() is deprecated. Use get() instead.', JLog::WARNING, 'deprecated');
 		// Cast to string, in case JREQUEST_ALLOWRAW was specified for mask
 		return (string) self::getVar($name, $default, $input, 'string', $mask);
 	}
 	
-	public static function getInt($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'int');
-	}
-	
 	public static function getUint($name, $default = '', $input = array())
 	{
+		JLog::add('FOFInput::getUint() is deprecated. Use get() instead.', JLog::WARNING, 'deprecated');
 		if(!version_compare(JVERSION, '1.6.0', 'ge')) {
 			return @abs((int) self::getVar($name, $default, $input, 'uint'));
 		} else {
@@ -99,23 +169,9 @@ class FOFInput
 		}
 	}
 	
-	public static function getFloat($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'float');
-	}
-	
-	public static function getAlnum($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'alnum');
-	}
-	
-	public static function getBase64($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'base64');
-	}
-	
 	public static function getHtml($name, $default = '', $input = array())
 	{
+		JLog::add('FOFInput::getHtml() is deprecated. Use get() instead.', JLog::WARNING, 'deprecated');
 		if(version_compare(JVERSION,'1.6.0','ge')) {
 			return self::getVar($name, $default, $input, 'html');
 		} else {
@@ -125,28 +181,17 @@ class FOFInput
 		}
 	}
 	
-	public static function getArray($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'array');
-	}
-	
-	public static function getPath($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'path');
-	}
-	
-	public static function getUsername($name, $default = '', $input = array())
-	{
-		return self::getVar($name, $default, $input, 'username');
-	}
-	
-	protected static function _stripSlashesRecursive($value)
-	{
-		$value = is_array($value) ? array_map(array('FOFInput', '_stripSlashesRecursive'), $value) : stripslashes($value);
-		return $value;
-	}
-	
-	protected static function _cleanVar($var, $mask = 0, $type = null)
+	/**
+	 * Custom filter implementation. Works better with arrays and allows the use
+	 * of a filter mask.
+	 * 
+	 * @param string $var
+	 * @param int $mask
+	 * @param string $type
+	 * 
+	 * @return mixed
+	 */
+	protected function _cleanVar($var, $mask = 0, $type = null)
 	{
 		if(is_array($var)) {
 			$temp = array();
@@ -176,10 +221,7 @@ class FOFInput
 		}
 		else
 		{
-			// Since no allow flags were set, we will apply the most strict filter to the variable
-			// $tags, $attr, $tag_method, $attr_method, $xss_auto use defaults.
-			$noHtmlFilter = JFilterInput::getInstance();
-			$var = $noHtmlFilter->clean($var, $type);
+			$this->filter->clean($var, $type);
 		}
 		return $var;
 	}
