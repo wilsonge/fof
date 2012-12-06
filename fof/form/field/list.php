eedd307f2fdb9e3058aa1e22919a7ff536b3591b
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-12-06 19:30:20 +0200

    Typo

--- fof/form/field/list.php
+++ fof/form/field/list.php
@@ -66,7 +66,7 @@ class FOFFormFieldList extends JFormFieldList implements FOFFormField
 	public function getStatic() {
 		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
 		
-		return '<span id="' . $this->id . '" ' . $class . '">' .
+		return '<span id="' . $this->id . '" ' . $class . '>' .
 			htmlspecialchars(self::getOptionName($this->getOptions(), $this->value), ENT_COMPAT, 'UTF-8') .
 			'</span>';
 	}
