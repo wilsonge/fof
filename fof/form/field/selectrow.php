5ad1f029ca0188b158b6952a6e71481ba1b9f8a3
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-12-09 11:36:52 +0200

    FOF Form field: selectrow, implementing a row selection checkbox

---
+++ fof/form/field/selectrow.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @package    FrameworkOnFramework
+ * @copyright  Copyright (C) 2010 - 2012 Akeeba Ltd. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see LICENSE.txt
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+if(!class_exists('JFormField')) {
+	require_once JPATH_LIBRARIES.'/joomla/form/fields/sessionhandler.php';
+}
+
+/**
+ * Form Field class for FOF
+ * Renders the checkbox in browse views which allows you to select rows
+ *
+ * @since       2.0
+ */
+class FOFFormFieldSelectrow extends JFormField implements FOFFormField
+{
+	protected $static;
+	
+	protected $repeatable;
+	
+	/**
+	 * Method to get certain otherwise inaccessible properties from the form field object.
+	 *
+	 * @param   string  $name  The property name for which to the the value.
+	 *
+	 * @return  mixed  The property value or null.
+	 *
+	 * @since   2.0
+	 */
+	public function __get($name)
+	{
+		switch($name) {
+			case 'static':
+				if(empty($this->static)) {
+					$this->static = $this->getStatic();
+				}
+
+				return $this->static;
+				break;
+				
+			case 'repeatable':
+				if(empty($this->repeatable)) {
+					$this->repeatable = $this->getRepeatable();
+				}
+
+				return $this->static;
+				break;
+				
+			default:
+				return parent::__get($name);
+		}
+	}
+	
+	protected function getInput()
+	{
+		throw new Exception(__CLASS__.' cannot be used in input forms');
+	}
+	
+	protected function getStatic()
+	{
+		throw new Exception(__CLASS__.' cannot be used in single item display forms');
+	}
+	
+	protected function getRepeatable()
+	{
+		static $counters = array();
+		
+		if (!($this->item instanceof FOFTable))
+		{
+			throw new Exception(__CLASS__.' needs a FOFTable to act upon');
+		}
+
+		// Get the counter based on the unique name of the form
+		$signature = $this->form->getName();
+		if (empty($counters[$signature]))
+		{
+			$counters[$signature] = 0;
+		}
+		
+		// Is this record checked out?
+		$checked_out = false;
+		$locked_by_field = $this->item->getColumnAlias('locked_by');
+		if(property_exists($this->item, $locked_by_field))
+		{
+			$locked_by = $this->item->$locked_by_field;
+			$checked_out = ($locked_by != 0);
+		}
+		
+		// Get the key id for this record
+		$key_field = $this->item->getKeyName();
+		$key_id = $this->item->$key_field;
+		
+		// Get the HTML
+		$counters[$signature]++;
+		return JHTML::_('grid.id', $counters[$signature], $key_id, $checked_out);
+	}
+}
