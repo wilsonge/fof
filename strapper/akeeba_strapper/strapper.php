afe243c189b3c6460278bc33038bfefcaa8afdeb
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2013-02-21 19:28:20 +0200

    Removing trailing whitespace

--- strapper/akeeba_strapper/strapper.php
+++ strapper/akeeba_strapper/strapper.php
@@ -3,55 +3,55 @@
  * Akeeba Strapper
  * A handy distribution of namespaced jQuery, jQuery UI and Twitter
  * Bootstrapper for use with Akeeba components.
- * 
+ *
  * @copyright (c) 2012-2013 Akeeba Ltd
  * @license GNU General Public License version 2 or later
  */
- 
+
 defined('_JEXEC') or die();
 
 if(!defined('FOF_INCLUDED')) {
 	include_once JPATH_SITE.'/libraries/fof/include.php';
 }
- 
+
 class AkeebaStrapper {
  	/** @var bool True when jQuery is already included */
  	public static $_includedJQuery = false;
- 	
+
  	/** @var bool True when jQuery UI is already included */
  	public static $_includedJQueryUI = false;
- 	
+
  	/** @var bool True when Bootstrap is already included */
  	public static $_includedBootstrap = false;
- 	
+
  	/** @var array List of URLs to Javascript files */
  	public static $scriptURLs = array();
- 	
+
  	/** @var array List of script definitions to include in the head */
  	public static $scriptDefs = array();
- 	
+
  	/** @var array List of URLs to CSS files */
  	public static $cssURLs = array();
- 	
+
 	/** @var array List of URLs to LESS files */
  	public static $lessURLs = array();
- 	
+
  	/** @var array List of CSS definitions to include in the head */
  	public static $cssDefs = array();
- 	
+
  	/** @var string The jQuery UI theme to use, default is 'smoothness' */
  	protected static $jqUItheme = 'smoothness';
- 	
+
 	/** @var string A query tag to append to CSS and JS files for versioning purposes */
 	public static $tag = null;
-	
+
 	/** @var bool Should I preload my Javascript/CSS files on Joomla! 2.5 or earlier? */
 	public static $preloadOnOldJoomla = true;
-	
+
 	/**
 	 * Is this something running under the CLI mode?
 	 * @staticvar bool|null $isCli
-	 * @return null 
+	 * @return null
 	 */
 	public static function isCli()
 	{
@@ -65,20 +65,20 @@ class AkeebaStrapper {
 				}
 			} catch(Exception $e) {
 				$isCLI = true;
-			}		
+			}
 		}
 		return $isCli;
 	}
-	
+
  	/**
  	 * Loads our namespaced jQuery, accessible through akeeba.jQuery
  	 */
  	public static function jQuery()
  	{
 		if(self::isCli()) return;
-		
+
  		self::$_includedJQuery = true;
- 		
+
 		if(version_compare(JVERSION, '3.0', 'lt')) {
 			// Joomla! 2.5 and earlier, load our own library
 			self::$scriptURLs[] = FOFTemplateUtils::parsePath('media://akeeba_strapper/js/akeebajq.js');
@@ -97,7 +97,7 @@ ENDSCRIPT;
 			JFactory::getDocument()->addScriptDeclaration($script);
 		}
  	}
- 	
+
  	/**
  	 * Sets the jQuery UI theme to use. It must be the name of a subdirectory of
  	 * media/akeeba_strapper/css or templates/<yourtemplate>/media/akeeba_strapper/css
@@ -107,24 +107,24 @@ ENDSCRIPT;
  	public static function setjQueryUItheme($theme)
  	{
 		if(self::isCli()) return;
-		
+
  		self::$jqUItheme = $theme;
  	}
- 	
+
 	/**
 	 * Loads our namespaced jQuery UI and its stylesheet
 	 */
  	public static function jQueryUI()
  	{
 		if(self::isCli()) return;
-		
+
  		if(!self::$_includedJQuery) {
  			self::jQuery();
  		}
- 	
+
  		self::$_includedJQueryUI = true;
  		$theme = self::$jqUItheme;
- 		
+
 		$url = FOFTemplateUtils::parsePath('media://akeeba_strapper/js/akeebajqui.js');
 		if(version_compare(JVERSION, '3.0', 'lt')) {
 			// Joomla! 2.5, use our magic loader
@@ -136,7 +136,7 @@ ENDSCRIPT;
 		}
  		self::$cssURLs[] = FOFTemplateUtils::parsePath("media://akeeba_strapper/css/$theme/theme.css");
  	}
- 	
+
  	/**
  	 * Loads our namespaced Twitter Bootstrap. You have to wrap the output you want style
  	 * with an element having the class akeeba-bootstrap added to it.
@@ -144,13 +144,13 @@ ENDSCRIPT;
  	public static function bootstrap()
  	{
 		if(self::isCli()) return;
-		
+
  		if(!self::$_includedJQuery) {
  			self::jQuery();
  		}
-		
+
 		self::$_includedBootstrap = true;
- 	
+
 		$altCss = array('media://akeeba_strapper/css/strapper.css');
 		if(version_compare(JVERSION, '3.0', 'lt'))
 		{
@@ -163,7 +163,7 @@ ENDSCRIPT;
 			self::$lessURLs[] = array('media://akeeba_strapper/less/bootstrap.j30.less', $altCss);
 		}
  	}
- 	
+
  	/**
  	 * Adds an arbitraty Javascript file.
  	 *
@@ -173,7 +173,7 @@ ENDSCRIPT;
  	{
  		self::$scriptURLs[] = FOFTemplateUtils::parsePath($path);
  	}
- 	
+
  	/**
  	 * Add inline Javascript
  	 *
@@ -183,7 +183,7 @@ ENDSCRIPT;
 	{
 		self::$scriptDefs[] = $script;
 	}
-	
+
 	/**
 	 * Adds an arbitraty CSS file.
 	 *
@@ -193,7 +193,7 @@ ENDSCRIPT;
  	{
  		self::$cssURLs[] = FOFTemplateUtils::parsePath($path);
  	}
- 	
+
 	/**
 	 * Adds an arbitraty LESS file.
 	 *
@@ -204,7 +204,7 @@ ENDSCRIPT;
  	{
  		self::$lessURLs[] = array($path, $altPaths);
  	}
- 	
+
 	/**
 	 * Add inline CSS
 	 *
@@ -215,7 +215,7 @@ ENDSCRIPT;
  		self::$cssDefs[] = $style;
  	}
  }
- 
+
  /**
   * This is a workaround which ensures that Akeeba's namespaced JavaScript and CSS will be loaded
   * wihtout being tampered with by any system pluign. Moreover, since we are loading first, we can
@@ -228,7 +228,7 @@ ENDSCRIPT;
   * Peace.
   */
  function AkeebaStrapperLoader()
- {	 
+ {
  	// If there are no script defs, just go to sleep
  	if(
 		empty(AkeebaStrapper::$scriptURLs) &&
@@ -239,7 +239,7 @@ ENDSCRIPT;
 	) {
 		return;
 	}
-	
+
 	// Get the query tag
 	$tag = AkeebaStrapper::$tag;
 	if(empty($tag)) {
@@ -247,13 +247,13 @@ ENDSCRIPT;
 	} else {
 		$tag = '?'.ltrim($tag,'?');
 	}
- 
+
  	$myscripts = '';
-	
+
 	if(version_compare(JVERSION, '3.0', 'lt') && AkeebaStrapper::$preloadOnOldJoomla) {
 		$buffer = JResponse::getBody();
 	}
-	
+
 	// Include Javascript files
  	if(!empty(AkeebaStrapper::$scriptURLs)) foreach(AkeebaStrapper::$scriptURLs as $url)
  	{
@@ -279,7 +279,7 @@ ENDSCRIPT;
 			JFactory::getDocument()->addScript($url.$tag);
 		}
  	}
- 	
+
 	// Include Javscript snippets
  	if(!empty(AkeebaStrapper::$scriptDefs))
  	{
@@ -298,16 +298,16 @@ ENDSCRIPT;
 			JFactory::getDocument()->addScriptDeclaration($myscripts);
 		}
  	}
-	
+
 	// Include LESS files
  	if(!empty(AkeebaStrapper::$lessURLs))
 	{
 		foreach(AkeebaStrapper::$lessURLs as $entry)
 		{
 			list($lessFile, $altFiles) = $entry;
-			
+
 			$url = FOFTemplateUtils::addLESS($lessFile, $altFiles, true);
-			
+
 			if(version_compare(JVERSION, '3.0', 'lt') && AkeebaStrapper::$preloadOnOldJoomla) {
 				if(empty($url))
 				{
@@ -351,7 +351,7 @@ ENDSCRIPT;
 			}
 		}
 	}
- 	
+
 	// Include CSS files
  	if(!empty(AkeebaStrapper::$cssURLs)) foreach(AkeebaStrapper::$cssURLs as $url)
  	{
@@ -361,7 +361,7 @@ ENDSCRIPT;
 			JFactory::getDocument()->addStyleSheet($url.$tag);
 		}
  	}
- 	
+
 	// Include style definitions
  	if(!empty(AkeebaStrapper::$cssDefs))
  	{
@@ -376,7 +376,7 @@ ENDSCRIPT;
  		}
  		$myscripts .= '</style>'."\n";
  	}
- 	
+
 	if(version_compare(JVERSION, '3.0', 'lt') && AkeebaStrapper::$preloadOnOldJoomla) {
 		$pos = strpos($buffer, "<head>");
 		if($pos > 0)
@@ -386,7 +386,7 @@ ENDSCRIPT;
 		}
 	}
  }
- 
+
 // Add our pseudo-plugin to the application event queue
 if(!AkeebaStrapper::isCli()) {
 	$app = JFactory::getApplication();
