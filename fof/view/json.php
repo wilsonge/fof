60d46137b883791bc667caedc9f05847cd4dfc91
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-05-12 10:30:59 +0300

    gh-19 Add support for HAL in FOFViewJson

--- fof/view/json.php
+++ fof/view/json.php
@@ -18,6 +18,13 @@ JLoader::import('joomla.application.component.view');
  */
 class FOFViewJson extends FOFViewHtml
 {
+	/**
+	 * When set to true we'll add hypermedia to the output, implementing the
+	 * HAL specification (http://stateless.co/hal_specification.html)
+	 *
+	 * @var   boolean
+	 */
+	public $useHypermedia = false;
 
 	protected function onDisplay($tpl = null)
 	{
@@ -65,7 +72,15 @@ class FOFViewJson extends FOFViewHtml
 		if ($hasFailed)
 		{
 			// Default JSON behaviour in case the template isn't there!
-			$json = json_encode($items);
+			if ($this->useHypermedia)
+			{
+				$haldocument = $this->_createDocumentWithHypermedia($items, $model);
+				$json = $haldocument->render('json');
+			}
+			else
+			{
+				$json = json_encode($items);
+			}
 
 			// JSONP support
 			$callback = $this->input->getVar('callback', null);
@@ -133,7 +148,15 @@ class FOFViewJson extends FOFViewHtml
 		if ($hasFailed)
 		{
 			// Default JSON behaviour in case the template isn't there!
-			$json = json_encode($item);
+			if ($this->useHypermedia)
+			{
+				$haldocument = $this->_createDocumentWithHypermedia($item, $model);
+				$json = $haldocument->render('json');
+			}
+			else
+			{
+				$json = json_encode($item);
+			}
 
 			// JSONP support
 			$callback = $this->input->get('callback', null);
@@ -158,4 +181,116 @@ class FOFViewJson extends FOFViewHtml
 		}
 	}
 
+	protected function _createDocumentWithHypermedia($data, $model = null)
+	{
+		// Create a new HAL document
+		if (is_array($data))
+		{
+			$count = count($data);
+		}
+		else
+		{
+			$count = null;
+		}
+
+		if ($count == 1)
+		{
+			reset($data);
+			$document = new FOFHalDocument(end($data));
+		}
+		else
+		{
+			$document = new FOFHalDocument($data);
+		}
+
+
+		// Create a self link
+		$uri = (string)(JUri::getInstance());
+		$uri = $this->_removeURIBase($uri);
+		$uri = JRoute::_($uri);
+		$document->addLink('self', new FOFHalLink($uri));
+
+		// Create relative links in a record list context
+		if (is_array($data) && ($model instanceof FOFModel))
+		{
+			$pagination = $model->getPagination();
+
+			if ($pagination->get('pages.total') > 1)
+			{
+				// Try to guess URL parameters and create a prototype URL
+				// NOTE: You are better off specialising this method
+				$protoUri = $this->_getPrototypeURIForPagination();
+
+				// The "first" link
+				$uri = clone $protoUri;
+				$uri->setVar('limitstart', 0);
+				$uri = JRoute::_((string)$uri);
+
+				$document->addLink('first', new FOFHalLink($uri));
+
+				// Do we need a "prev" link?
+				if ($pagination->get('pages.current') > 1)
+				{
+					$prevPage = $pagination->get('pages.current') - 1;
+					$limitstart = ($prevPage - 1) * $pagination->limit;
+					$uri = clone $protoUri;
+					$uri->setVar('limitstart', $limitstart);
+					$uri = JRoute::_((string)$uri);
+
+					$document->addLink('prev', new FOFHalLink($uri));
+				}
+
+				// Do we need a "next" link?
+				if ($pagination->get('pages.current') < $pagination->get('pages.total'))
+				{
+					$nextPage = $pagination->get('pages.current') + 1;
+					$limitstart = ($nextPage - 1) * $pagination->limit;
+					$uri = clone $protoUri;
+					$uri->setVar('limitstart', $limitstart);
+					$uri = JRoute::_((string)$uri);
+
+					$document->addLink('next', new FOFHalLink($uri));
+				}
+
+				// The "last" link?
+				$lastPage = $pagination->get('pages.total');
+				$limitstart = ($lastPage - 1) * $pagination->limit;
+				$uri = clone $protoUri;
+				$uri->setVar('limitstart', $limitstart);
+				$uri = JRoute::_((string)$uri);
+
+				$document->addLink('last', new FOFHalLink($uri));
+			}
+		}
+
+		return $document;
+	}
+
+	protected function _removeURIBase($uri)
+	{
+		static $root = null, $rootlen = 0;
+
+		if (is_null($root))
+		{
+			$root = rtrim(JURI::base(),'/');
+			$rootlen = strlen($root);
+		}
+
+		if (substr($uri, 0, $rootlen) == $root)
+		{
+			$uri = substr($uri, $rootlen);
+		}
+
+		return ltrim($uri, '/');
+	}
+
+	protected function _getPrototypeURIForPagination()
+	{
+		$protoUri = new JUri('index.php');
+		$protoUri->setQuery($this->input->getData());
+		$protoUri->delVar('savestate');
+		$protoUri->delVar('base_path');
+
+		return $protoUri;
+	}
 }
