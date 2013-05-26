d8f6f90df2378b3516315b757498b6298107227a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-05-26 12:36:03 +0300

    More of the same

--- fof/toolbar/toolbar.php
+++ fof/toolbar/toolbar.php
@@ -7,6 +7,13 @@
 // Protect from unauthorized access
 defined('_JEXEC') or die();
 
+/**
+ * The Toolbar class renders the back-end component title area and the back-
+ * and front-end toolbars.
+ *
+ * @package  FrameworkOnFramework
+ * @since    1.0
+ */
 class FOFToolbar
 {
 
@@ -29,12 +36,12 @@ class FOFToolbar
 	protected $renderFrontendButtons = false;
 
 	/**
+	 * Gets an instance of a component's toolbar
 	 *
-	 * @staticvar array $instances
-	 * @param type $option
-	 * @param type $view
-	 * @param type $config
-	 * @return FOFToolbar
+	 * @param   string  $option  The name of the component
+	 * @param   array   $config  The configuration array for the component
+	 *
+	 * @return  FOFToolbar  The toolbar instance for the component
 	 */
 	public static function &getAnInstance($option = null, $config = array())
 	{
@@ -51,6 +58,7 @@ class FOFToolbar
 		}
 
 		$hash = $option;
+
 		if (!array_key_exists($hash, $instances))
 		{
 			if (array_key_exists('input', $config))
@@ -66,13 +74,15 @@ class FOFToolbar
 			}
 			else
 			{
-				$input = new FOFInput();
+				$input = new FOFInput;
 			}
+
 			$config['option'] = !is_null($option) ? $option : $input->getCmd('option', 'com_foobar');
 			$input->set('option', $config['option']);
 			$config['input'] = $input;
 
 			$className = ucfirst(str_replace('com_', '', $config['option'])) . 'Toolbar';
+
 			if (!class_exists($className))
 			{
 				$componentPaths = FOFPlatform::getInstance()->getComponentBaseDirs($config['option']);
@@ -83,6 +93,7 @@ class FOFToolbar
 					$componentPaths['alt'],
 					$componentPaths['alt'] . '/toolbars'
 				);
+
 				if (array_key_exists('searchpath', $config))
 				{
 					array_unshift($searchPaths, $config['searchpath']);
@@ -111,6 +122,11 @@ class FOFToolbar
 		return $instances[$hash];
 	}
 
+	/**
+	 * Public cinstructor
+	 *
+	 * @param   array  $config  The configuration array of the component
+	 */
 	public function __construct($config = array())
 	{
 		// Make sure $config is an array
@@ -133,15 +149,18 @@ class FOFToolbar
 		}
 		else
 		{
-			$this->input = new FOFInput();
+			$this->input = new FOFInput;
 		}
 
 		// Get the default values for the component and view names
 		$this->component = $this->input->getCmd('option', 'com_foobar');
 
 		// Overrides from the config
+
 		if (array_key_exists('option', $config))
+		{
 			$this->component = $config['option'];
+		}
 
 		$this->input->set('option', $this->component);
 
@@ -157,15 +176,20 @@ class FOFToolbar
 
 		// Save front-end toolbar and submenu rendering flags if present in the config
 		if (array_key_exists('renderFrontendButtons', $config))
+		{
 			$this->renderFrontendButtons = $config['renderFrontendButtons'];
-		if (array_key_exists('renderFrontendSubmenu', $config))
-			$this->renderFrontendSubmenu = $config['renderFrontendSubmenu'];
+		}
 
-		//if not in the administrative area, load the JToolbarHelper
+		if (array_key_exists('renderFrontendSubmenu', $config))
+		{
+			$this->renderFrontendSubmenu = $config['renderFrontendSubmenu'];
+		}
+
+		// If not in the administrative area, load the JToolbarHelper
 		if (!FOFPlatform::getInstance()->isBackend())
 		{
-			//pretty ugly require...
-			require_once(JPATH_ROOT . '/administrator/includes/toolbar.php');
+			// Pretty ugly require...
+			require_once JPATH_ROOT . '/administrator/includes/toolbar.php';
 
 			// Things to do if we have to render a front-end toolbar
 			if ($this->renderFrontendButtons)
@@ -182,6 +206,15 @@ class FOFToolbar
 		$this->perms = $perms;
 	}
 
+	/**
+	 * Renders the toolbar for the current view and task
+	 *
+	 * @param   string    $view   The view of the component
+	 * @param   string    $task   The exact task of the view
+	 * @param   FOFInput  $input  An optional input object used to determine the defaults
+	 *
+	 * @return  void
+	 */
 	public function renderToolbar($view = null, $task = null, $input = null)
 	{
 		if (!empty($input))
@@ -191,14 +224,23 @@ class FOFToolbar
 		}
 
 		// If there is a render.toolbar=0 in the URL, do not render a toolbar
+
 		if (!$this->input->getBool('render.toolbar', true))
+		{
 			return;
+		}
 
 		// Get the view and task
+
 		if (empty($view))
+		{
 			$view = $this->input->getCmd('view', 'cpanel');
+		}
+
 		if (empty($task))
+		{
 			$task = $this->input->getCmd('task', 'default');
+		}
 
 		$this->view = $view;
 		$this->task = $task;
@@ -207,6 +249,7 @@ class FOFToolbar
 
 		// Check for an onViewTask method
 		$methodName = 'on' . ucfirst($view) . ucfirst($task);
+
 		if (method_exists($this, $methodName))
 		{
 			return $this->$methodName();
@@ -214,6 +257,7 @@ class FOFToolbar
 
 		// Check for an onView method
 		$methodName = 'on' . ucfirst($view);
+
 		if (method_exists($this, $methodName))
 		{
 			return $this->$methodName();
@@ -221,6 +265,7 @@ class FOFToolbar
 
 		// Check for an onTask method
 		$methodName = 'on' . ucfirst($task);
+
 		if (method_exists($this, $methodName))
 		{
 			return $this->$methodName();
@@ -234,6 +279,8 @@ class FOFToolbar
 
 	/**
 	 * Renders the toolbar for the component's Control Panel page
+	 *
+	 * @return  void
 	 */
 	public function onCpanelsBrowse()
 	{
@@ -243,7 +290,9 @@ class FOFToolbar
 		}
 
 		if (!FOFPlatform::getInstance()->isBackend() && !$this->renderFrontendButtons)
+		{
 			return;
+		}
 
 		$option = $this->input->getCmd('option', 'com_foobar');
 
@@ -253,10 +302,12 @@ class FOFToolbar
 
 	/**
 	 * Renders the toolbar for the component's Browse pages (the plural views)
+	 *
+	 * @return  void
 	 */
 	public function onBrowse()
 	{
-		//on frontend, buttons must be added specifically
+		// On frontend, buttons must be added specifically
 
 		if (FOFPlatform::getInstance()->isBackend() || $this->renderFrontendSubmenu)
 		{
@@ -264,7 +315,9 @@ class FOFToolbar
 		}
 
 		if (!FOFPlatform::getInstance()->isBackend() && !$this->renderFrontendButtons)
+		{
 			return;
+		}
 
 		// Set toolbar title
 		$option = $this->input->getCmd('option', 'com_foobar');
@@ -272,6 +325,7 @@ class FOFToolbar
 		JToolBarHelper::title(JText::_(strtoupper($option)) . ' &ndash; <small>' . JText::_($subtitle_key) . '</small>', str_replace('com_', '', $option));
 
 		// Add toolbar buttons
+
 		if ($this->perms->create)
 		{
 			if (version_compare(JVERSION, '3.0', 'ge'))
@@ -283,6 +337,7 @@ class FOFToolbar
 				JToolBarHelper::addNewX();
 			}
 		}
+
 		if ($this->perms->edit)
 		{
 			if (version_compare(JVERSION, '3.0', 'ge'))
@@ -294,6 +349,7 @@ class FOFToolbar
 				JToolBarHelper::editListX();
 			}
 		}
+
 		if ($this->perms->create || $this->perms->edit)
 		{
 			JToolBarHelper::divider();
@@ -305,6 +361,7 @@ class FOFToolbar
 			JToolBarHelper::unpublishList();
 			JToolBarHelper::divider();
 		}
+
 		if ($this->perms->delete)
 		{
 			$msg = JText::_($this->input->getCmd('option', 'com_foobar') . '_CONFIRM_DELETE');
@@ -314,10 +371,12 @@ class FOFToolbar
 
 	/**
 	 * Renders the toolbar for the component's Read pages
+	 *
+	 * @return  void
 	 */
 	public function onRead()
 	{
-		//on frontend, buttons must be added specifically
+		// On frontend, buttons must be added specifically
 
 		if (FOFPlatform::getInstance()->isBackend() || $this->renderFrontendSubmenu)
 		{
@@ -325,7 +384,9 @@ class FOFToolbar
 		}
 
 		if (!FOFPlatform::getInstance()->isBackend() && !$this->renderFrontendButtons)
+		{
 			return;
+		}
 
 		$option = $this->input->getCmd('option', 'com_foobar');
 		$componentName = str_replace('com_', '', $option);
@@ -338,11 +399,18 @@ class FOFToolbar
 		JToolBarHelper::back();
 	}
 
+	/**
+	 * Renders the toolbar for the component's Add pages
+	 *
+	 * @return  void
+	 */
 	public function onAdd()
 	{
-		//on frontend, buttons must be added specifically
+		// On frontend, buttons must be added specifically
 		if (!FOFPlatform::getInstance()->isBackend() && !$this->renderFrontendButtons)
+		{
 			return;
+		}
 
 		$option = $this->input->getCmd('option', 'com_foobar');
 		$componentName = str_replace('com_', '', $option);
@@ -358,17 +426,26 @@ class FOFToolbar
 		JToolBarHelper::cancel();
 	}
 
+	/**
+	 * Renders the toolbar for the component's Edit pages
+	 *
+	 * @return  void
+	 */
 	public function onEdit()
 	{
-		//on frontend, buttons must be added specifically
+		// On frontend, buttons must be added specifically
 		if (!FOFPlatform::getInstance()->isBackend() && !$this->renderFrontendButtons)
+		{
 			return;
+		}
 
 		$this->onAdd();
 	}
 
 	/**
 	 * Removes all links from the link bar
+	 *
+	 * @return  void
 	 */
 	public function clearLinks()
 	{
@@ -377,7 +454,8 @@ class FOFToolbar
 
 	/**
 	 * Get the link bar's link definitions
-	 * @return array
+	 *
+	 * @return  array
 	 */
 	public function &getLinks()
 	{
@@ -387,11 +465,13 @@ class FOFToolbar
 	/**
 	 * Append a link to the link bar
 	 *
-	 * @param string $name The text of the link
-	 * @param string|null $link The link to render; set to null to render a separator
-	 * @param bool $active True if it's an active link
-	 * @param string|null $icon Icon class (used by some renderers, like the Bootstrap renderer)
-	 * @param string|null $parent The parent element (referenced by name)) Thsi will create a dropdown list
+	 * @param   string       $name    The text of the link
+	 * @param   string|null  $link    The link to render; set to null to render a separator
+	 * @param   boolean      $active  True if it's an active link
+	 * @param   string|null  $icon    Icon class (used by some renderers, like the Bootstrap renderer)
+	 * @param   string|null  $parent  The parent element (referenced by name)) Thsi will create a dropdown list
+	 *
+	 * @return  void
 	 */
 	public function appendLink($name, $link = null, $active = false, $icon = null, $parent = '')
 	{
@@ -401,6 +481,7 @@ class FOFToolbar
 			'active' => $active,
 			'icon'	 => $icon
 		);
+
 		if (empty($parent))
 		{
 			$this->linkbar[$name] = $linkDefinition;
@@ -417,6 +498,7 @@ class FOFToolbar
 			else
 			{
 				$parentElement = $this->linkbar[$parent];
+
 				if (!array_key_exists('dropdown', $parentElement) && !empty($parentElement['link']))
 				{
 					$newSubElement = $parentElement;
@@ -434,10 +516,12 @@ class FOFToolbar
 	/**
 	 * Prefixes (some people erroneously call this "prepend" – there is no such word) a link to the link bar
 	 *
-	 * @param string $name The text of the link
-	 * @param string|null $link The link to render; set to null to render a separator
-	 * @param bool $active True if it's an active link
-	 * @param srting|null $icon Icon class (used by some renderers, like the Bootstrap renderer)
+	 * @param   string       $name    The text of the link
+	 * @param   string|null  $link    The link to render; set to null to render a separator
+	 * @param   boolean      $active  True if it's an active link
+	 * @param   string|null  $icon    Icon class (used by some renderers, like the Bootstrap renderer)
+	 *
+	 * @return  void
 	 */
 	public function prefixLink($name, $link = null, $active = false, $icon = null)
 	{
@@ -452,12 +536,17 @@ class FOFToolbar
 
 	/**
 	 * Renders the submenu (toolbar links) for all detected views of this component
+	 *
+	 * @return  void
 	 */
 	protected function renderSubmenu()
 	{
 		$views = $this->getMyViews();
+
 		if (empty($views))
+		{
 			return;
+		}
 
 		$activeView = $this->input->getCmd('view', 'cpanel');
 
@@ -465,10 +554,12 @@ class FOFToolbar
 		{
 			// Get the view name
 			$key = strtoupper($this->component) . '_TITLE_' . strtoupper($view);
+
 			if (strtoupper(JText::_($key)) == $key)
 			{
 				$altview = FOFInflector::isPlural($view) ? FOFInflector::singularize($view) : FOFInflector::pluralize($view);
 				$key2 = strtoupper($this->component) . '_TITLE_' . strtoupper($altview);
+
 				if (strtoupper(JText::_($key2)) == $key2)
 				{
 					$name = ucfirst($view);
@@ -494,7 +585,7 @@ class FOFToolbar
 	/**
 	 * Automatically detects all views of the component
 	 *
-	 * @return array
+	 * @return  array  A list of all views, in the order to be displayed in the toolbar submenu
 	 */
 	protected function getMyViews()
 	{
@@ -511,26 +602,33 @@ class FOFToolbar
 		$allFolders = JFolder::folders($searchPath);
 
 		if (!empty($allFolders))
+		{
 			foreach ($allFolders as $folder)
 			{
 				$view = $folder;
 
 				// View already added
 				if (in_array(FOFInflector::pluralize($view), $t_views))
+				{
 					continue;
+				}
 
 				// Do we have a 'skip.xml' file in there?
 				$files = JFolder::files($searchPath . '/' . $view, '^skip\.xml$');
-				if (!empty($files))
-					continue;
 
-				//Do we have extra information about this view? (ie. ordering)
+				if (!empty($files))
+				{
+					continue;
+				}
+
+				// Do we have extra information about this view? (ie. ordering)
 				$meta = JFolder::files($searchPath . '/' . $view, '^metadata\.xml$');
 
-				//Not found, do we have it inside the plural one?
+				// Not found, do we have it inside the plural one?
 				if (!$meta)
 				{
 					$plural = FOFInflector::pluralize($view);
+
 					if (in_array($plural, $allFolders))
 					{
 						$view = $plural;
@@ -547,28 +645,34 @@ class FOFToolbar
 				else
 				{
 					// Next place. It's ok since the index are 0-based and count is 1-based
+
 					if (!isset($to_order))
+					{
 						$to_order = array();
+					}
+
 					$order = count($to_order);
 				}
 
 				$view = FOFInflector::pluralize($view);
 
-				$t_view = new stdClass();
+				$t_view = new stdClass;
 				$t_view->ordering = $order;
 				$t_view->view = $view;
 
 				$to_order[] = $t_view;
 				$t_views[] = $view;
 			}
+		}
 
 		JArrayHelper::sortObjects($to_order, 'ordering');
 		$views = JArrayHelper::getColumn($to_order, 'view');
 
-		//if not using the metadata file, let's put the cpanel view on top
+		// If not using the metadata file, let's put the cpanel view on top
 		if (!$using_meta)
 		{
 			$cpanel = array_search('cpanels', $views);
+
 			if ($cpanel !== false)
 			{
 				unset($views[$cpanel]);
@@ -582,7 +686,7 @@ class FOFToolbar
 	/**
 	 * Return the front-end toolbar rendering flag
 	 *
-	 * @return bool
+	 * @return  boolean
 	 */
 	public function getRenderFrontendButtons()
 	{
@@ -592,7 +696,7 @@ class FOFToolbar
 	/**
 	 * Return the front-end submenu rendering flag
 	 *
-	 * @return bool
+	 * @return  boolean
 	 */
 	public function getRenderFrontendSubmenu()
 	{
