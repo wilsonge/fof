wilsonge/fof

commit cec12cd7f807b39d6d06337962d03f215da4016d

I screwed up the HTML view (@mbabker look, I am doing a PR from my JaB13 session!)

fof/view/html.php
@@ -110,15 +110,6 @@ class FOFViewHtml extends FOFViewRaw
 		$view = $this->input->getCmd('view', 'cpanel');
 		$task = $this->getModel()->getState('task', 'browse');
 
-		// Don't load the toolbar on CLI
-
-		if (!FOFPlatform::getInstance()->isCli())
-		{
-			$toolbar = FOFToolbar::getAnInstance($this->input->getCmd('option', 'com_foobar'), $this->config);
-			$toolbar->perms = $this->perms;
-			$toolbar->renderToolbar($view, $task, $this->input);
-		}
-
 		$renderer = $this->getRenderer();
 
 		if (!($renderer instanceof FOFRenderAbstract))

commit 46e88811e8708ca236979ed2de91a514e7ce0799

Fixing bugs introduced by commit cec12cd7f807b39d6d06337962d03f215da4016d

fof/view/form.php
@@ -60,11 +60,6 @@ class FOFViewForm extends FOFViewHtml
 			return;
 		}
 
-		// Render the toolbar
-		$toolbar = FOFToolbar::getAnInstance($this->input->getCmd('option', 'com_foobar'), $this->config);
-		$toolbar->perms = $this->perms;
-		$toolbar->renderToolbar($this->input->getCmd('view', 'cpanel'), $task, $this->input);
-
 		// Show the view
 		// -- Output HTML before the view template
 		$this->preRender();

fof/view/html.php
@@ -110,6 +110,15 @@ class FOFViewHtml extends FOFViewRaw
 		$view = $this->input->getCmd('view', 'cpanel');
 		$task = $this->getModel()->getState('task', 'browse');
 
+		// Don't load the toolbar on CLI
+
+		if (!FOFPlatform::getInstance()->isCli())
+		{
+			$toolbar = FOFToolbar::getAnInstance($this->input->getCmd('option', 'com_foobar'), $this->config);
+			$toolbar->perms = $this->perms;
+			$toolbar->renderToolbar($view, $task, $this->input);
+		}
+
 		$renderer = $this->getRenderer();
 
 		if (!($renderer instanceof FOFRenderAbstract))