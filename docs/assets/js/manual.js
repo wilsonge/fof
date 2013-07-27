57060b500cfd2f4342c4b2e0e8b6ede6466bd30b
wilsonge <georgejameswilson@googlemail.com>
2013-07-27 09:57:04 +0100

    Change to en-US so stuff works

--- docs/assets/js/manual.js
+++ docs/assets/js/manual.js
@@ -1,6 +1,6 @@
 var populateWindow = function(link) {
 	var markdownRequest = new Request({
-		"url": here + 'docs/manual/en-GB/' + link,
+		"url": here + 'docs/manual/en-US/' + link,
 		"method": "get",
 		"onSuccess": function(response) {
 			$('docwin').set('html', marked(response));
@@ -32,7 +32,7 @@ var populateWindow = function(link) {
 
 var populateMenu = function() {
 	var markdownRequest = new Request({
-		"url": here + 'docs/manual/en-GB/menu.md',
+		"url": here + 'docs/manual/en-US/menu.md',
 		"method": "get",
 		"onSuccess": function (response) {
 			$('doc-menu').set('html', marked(response));
