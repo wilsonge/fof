bbc1d01a32a9e72e03be7ac46c0f9666700afb11
Jurian Even <jurian@twentronix.com>
2013-11-27 07:18:43 +0100

    [Strapper] PHP 5.2 compatibility

--- strapper/akeeba_strapper/strapper.php
+++ strapper/akeeba_strapper/strapper.php
@@ -16,7 +16,7 @@ if (!defined('FOF_INCLUDED'))
     include_once JPATH_SITE . '/libraries/fof/include.php';
 }
 
-require_once __DIR__ . '/version.php';
+require_once dirname(__FILE__) . '/version.php';
 
 class AkeebaStrapper
 {
