035eccfc83fc92c19b69b2fb09b79cfe5a7a5476
Davide Tampellini <tampe125@gmail.com>
2013-12-30 10:00:13 +0100

    Fixed typo (method name is isFrontend, not isSite )

--- fof/controller/controller.php
+++ fof/controller/controller.php
@@ -1036,7 +1036,7 @@ class FOFController extends FOFUtilsObject
 		// Display the view
 		$conf = FOFPlatform::getInstance()->getConfig();
 
-		if (FOFPlatform::getInstance()->isSite() && $cachable && ($viewType != 'feed') && $conf->get('caching') >= 1)
+		if (FOFPlatform::getInstance()->isFrontend() && $cachable && ($viewType != 'feed') && $conf->get('caching') >= 1)
 		{
 			// Get a JCache object
 			$option = $this->input->get('option', 'com_foobar', 'cmd');
