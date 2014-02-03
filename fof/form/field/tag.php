35993c4c3cc6b7785907d8354b5b1243be50984c
Eighke <eighke@multi-site.net>
2014-02-03 18:51:33 +0700

    Wrong requirement for FOFFormFieldTag, FOFFormFieldModel and FOFFormFieldCaptcha

--- fof/form/field/tag.php
+++ fof/form/field/tag.php
@@ -8,10 +8,7 @@
 // Protect from unauthorized access
 defined('FOF_INCLUDED') or die;
 
-if (!class_exists('JFormFieldAccessLevel'))
-{
-	require_once JPATH_LIBRARIES . '/joomla/form/fields/accesslevel.php';
-}
+JFormHelper::loadFieldClass('tag');
 
 /**
  * Form Field class for FOF
