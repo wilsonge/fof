6cbd1efc45212245840060b3eac1316da933b1a6
Davide Tampellini <tampe125@gmail.com>
2014-04-01 10:33:43 +0200

    Added tests for FOFController::createFilename

--- tests/unit/suites/fof/controller/controllerDataprovider.php
+++ tests/unit/suites/fof/controller/controllerDataprovider.php
@@ -21,11 +21,50 @@ class ControllerDataprovider
                 'type'  => 'controller',
                 'parts' => array(
                     'name'   => 'foobar',
+                    'format' => 'html'
+                )
+            ),
+            array(
+                'filename' => 'foobar.php'
+            )
+        );
+
+        $data[] = array(
+            array(
+                'type'  => 'controller',
+                'parts' => array(
+                    'name'   => 'foobar',
                     'format' => 'raw'
                 )
             ),
             array(
                 'filename' => 'foobar.raw.php'
+            )
+        );
+
+        $data[] = array(
+            array(
+                'type'  => 'view',
+                'parts' => array(
+                    'name' => 'foobar',
+                    'type' => 'html'
+                )
+            ),
+            array(
+                'filename' => 'foobar/view.html.php'
+            )
+        );
+
+        $data[] = array(
+            array(
+                'type'  => 'view',
+                'parts' => array(
+                    'name' => 'foobar',
+                    'type' => 'raw'
+                )
+            ),
+            array(
+                'filename' => 'foobar/view.raw.php'
             )
         );
 
