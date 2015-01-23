a97b822b18536a0fd779756cd76a1a44674bd5ee
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-01-23 18:58:13 +0200

    Custom exceptions for the Dispatcher package

--- fof/Dispatcher/Dispatcher.php
+++ fof/Dispatcher/Dispatcher.php
@@ -9,6 +9,7 @@ namespace FOF30\Dispatcher;
 
 use FOF30\Container\Container;
 use FOF30\Controller\Controller;
+use FOF30\Dispatcher\Exception\AccessForbidden;
 use FOF30\TransparentAuthentication\TransparentAuthentication;
 
 defined('_JEXEC') or die;
@@ -149,7 +150,7 @@ class Dispatcher
 
 			$this->transparentAuthenticationLogout();
 
-			throw new \Exception(\JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'), 403);
+			throw new AccessForbidden;
 		}
 
 		// Get and execute the controller
@@ -174,7 +175,7 @@ class Dispatcher
 
 			$this->transparentAuthenticationLogout();
 
-			throw new \Exception(\JText::_('JLIB_APPLICATION_ERROR_ACCESS_FORBIDDEN'), 403);
+			throw new AccessForbidden;
 		}
 
 		$this->transparentAuthenticationLogout();
