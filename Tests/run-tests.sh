344e03f8f23d1a58bfd6e40e00381fa4a400d279
Davide Tampellini <tampe125@gmail.com>
2015-02-13 10:50:59 +0100

    Updated the shell script in order to run random tests

--- Tests/run-tests.sh
+++ Tests/run-tests.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-php `pwd`/../vendor/phpunit/phpunit/phpunit -c ../phpunit.xml "$@"
+
+if [ "$1" ]
+then
+	if [ $1 == 'native' ]
+	then
+		../vendor/bin/phpunit-randomizer -c ../phpunit.xml ../Tests/
+	else
+		../vendor/bin/phpunit-randomizer --order seed:$1 -c ../phpunit.xml ../Tests/
+	fi
+else
+	../vendor/bin/phpunit-randomizer --order rand -c ../phpunit.xml ../Tests/
+fi
