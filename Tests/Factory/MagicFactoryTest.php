c13083721cd4c3777ec7f8a9a9bd957e2bcdf248
Davide Tampellini <tampe125@gmail.com>
2015-03-25 15:46:10 +0100

    Added tests for the MagicFactory class

---
+++ Tests/Factory/MagicFactoryTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @package        FOF
+ * @copyright      2014 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license        GNU GPL version 3 or later
+ */
+
+namespace FOF30\Tests\Factory;
+
+use FOF30\Factory\MagicFactory;
+use FOF30\Tests\Helpers\FOFTestCase;
+use FOF30\Tests\Helpers\TestContainer;
+
+require_once 'MagicFactoryDataprovider.php';
+
+/**
+ * @covers      FOF30\Factory\MagicFactory::<protected>
+ * @covers      FOF30\Factory\MagicFactory::<private>
+ * @package     FOF30\Tests\Factory
+ */
+class MagicFactoryTest extends FOFTestCase
+{
+    /**
+     * @group           MagicFactory
+     * @covers          FOF30\Factory\MagicFactory::controller
+     * @dataProvider    MagicFactoryDataprovider::getTestController
+     */
+    public function testController($test, $check)
+    {
+        $msg   = 'MagicFactory::controller %s - Case: '.$check['case'];
+
+        $factory = new MagicFactory(static::$container);
+
+        $result = $factory->controller($test['view']);
+
+        $this->assertInstanceOf($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
+    }
+
+    /**
+     * @group           MagicFactory
+     * @covers          FOF30\Factory\MagicFactory::model
+     * @dataProvider    MagicFactoryDataprovider::getTestModel
+     */
+    public function testModel($test, $check)
+    {
+        $msg   = 'MagicFactory::model %s - Case: '.$check['case'];
+
+        $factory = new MagicFactory(static::$container);
+
+        $result = $factory->model($test['view']);
+
+        $this->assertInstanceOf($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
+    }
+
+    /**
+     * @group           MagicFactory
+     * @covers          FOF30\Factory\MagicFactory::view
+     * @dataProvider    MagicFactoryDataprovider::getTestView
+     */
+    public function testView($test, $check)
+    {
+        $msg   = 'MagicFactory::view %s - Case: '.$check['case'];
+
+        $platform = static::$container->platform;
+        $platform::$template = 'fake_test_template';
+        $platform::$uriBase  = 'www.example.com';
+
+        $factory = new MagicFactory(static::$container);
+
+        $result = $factory->view($test['view']);
+
+        $this->assertInstanceOf($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
+    }
+
+    /**
+     * @group           MagicFactory
+     * @covers          FOF30\Factory\MagicFactory::dispatcher
+     * @dataProvider    MagicFactoryDataprovider::getTestDispatcher
+     */
+    public function testDispatcher($test, $check)
+    {
+        $msg   = 'MagicFactory::dispatcher %s - Case: '.$check['case'];
+
+        $container = new TestContainer(array(
+            'backEndPath' => JPATH_TESTS.'/Stubs/Fakeapp/Admin'
+        ));
+
+        $platform = $container->platform;
+        $platform::$isAdmin = $test['backend'];
+
+        // Required so we force FOF to read the fof.xml file
+        $dummy = $container->appConfig;
+
+        $factory = new MagicFactory($container);
+
+        $result = $factory->dispatcher();
+
+        $this->assertInstanceOf($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
+    }
+
+    /**
+     * @group           MagicFactory
+     * @covers          FOF30\Factory\MagicFactory::transparentAuthentication
+     * @dataProvider    MagicFactoryDataprovider::getTestTransparentAuthentication
+     */
+    public function testTransparentAuthentication($test, $check)
+    {
+        $msg   = 'MagicFactory::transparentAuthentication %s - Case: '.$check['case'];
+
+        $container = new TestContainer(array(
+            'backEndPath' => JPATH_TESTS.'/Stubs/Fakeapp/Admin'
+        ));
+
+        $platform = $container->platform;
+        $platform::$isAdmin = $test['backend'];
+
+        // Required so we force FOF to read the fof.xml file
+        $dummy = $container->appConfig;
+
+        $factory = new MagicFactory($container);
+
+        $result = $factory->transparentAuthentication();
+
+        $this->assertInstanceOf($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
+    }
+}
---
+++ Tests/Stubs/Fakeapp/Site/View/Foobars/Html.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Fakeapp\Site\View\Foobars;
+
+use FOF30\View\View;
+
+class Html extends View
+{
+
+}
