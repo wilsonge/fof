e662d758a03bc20125e49bbf99acdf608d6e89e6
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-04-14 19:41:29 +0300

    User field: parse field tags

--- fof/Form/Field/User.php
+++ fof/Form/Field/User.php
@@ -196,7 +196,7 @@ class User extends \JFormFieldUser implements FieldInterface
 		{
 			$link_url = 'index.php?option=com_users&task=user.edit&id=[USER:ID]';
 		}
-		else
+		elseif (!$link_url)
 		{
 			// If no link is defined in the front-end, we can't create a
 			// default link. Therefore, show no link.
@@ -217,6 +217,8 @@ class User extends \JFormFieldUser implements FieldInterface
 			{
 				$link_url = str_replace($key, $value, $link_url);
 			}
+
+			$link_url = $this->parseFieldTags($link_url);
 		}
 
 		// Get the avatar image, if necessary
@@ -318,5 +320,56 @@ class User extends \JFormFieldUser implements FieldInterface
 		$html .= '</div>';
 
 		return $html;
+	}
+
+	/**
+	 * Replace string with tags that reference fields
+	 *
+	 * @param   string  $text  Text to process
+	 *
+	 * @return  string         Text with tags replace
+	 */
+	protected function parseFieldTags($text)
+	{
+		$ret = $text;
+
+		// Replace [ITEM:ID] in the URL with the item's key value (usually:
+		// the auto-incrementing numeric ID)
+		if (is_null($this->item))
+		{
+			$this->item = $this->form->getModel();
+		}
+
+		$keyfield = $this->item->getKeyName();
+		$replace  = $this->item->$keyfield;
+		$ret = str_replace('[ITEM:ID]', $replace, $ret);
+
+		// Replace the [ITEMID] in the URL with the current Itemid parameter
+		$ret = str_replace('[ITEMID]', $this->form->getContainer()->input->getInt('Itemid', 0), $ret);
+
+		// Replace other field variables in the URL
+		$fields = $this->item->getTableFields();
+
+		foreach ($fields as $fielddata)
+		{
+			$fieldname = $fielddata->Field;
+
+			if (empty($fieldname))
+			{
+				$fieldname = $fielddata->column_name;
+			}
+
+			$search    = '[ITEM:' . strtoupper($fieldname) . ']';
+			$replace   = $this->item->$fieldname;
+
+			if (!is_string($replace))
+			{
+				continue;
+			}
+
+			$ret  = str_replace($search, $replace, $ret);
+		}
+
+		return $ret;
 	}
 }
