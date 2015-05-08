8af91aac6175833217880ef87d72723da4b1a368
photodude <photodude@users.noreply.github.com>
2015-05-08 09:27:59 -0600

    Correct the location for enviroments
    - based on commit 5874702f18534a64e7c52e3dc067c6431b7fddf3

--- Tests/config_travis.php
+++ Tests/config_travis.php
@@ -5,9 +5,16 @@
  * @copyright   Copyright (C) 2010 - 2015 Akeeba Ltd. All rights reserved.
  * @license	    GNU General Public License version 2 or later; see LICENSE.txt
  *
- * Configuration for running the unit tests
+ * Configuration for running the unit tests on travis
  */
+ 
 $fofTestConfig = array(
 	// Point to a path where a Joomla! 3.x site is stored. It's our guinea pig!
-	'site_root'		=> './Tests/environments',
+	'3.4' => realpath(__DIR__.'/environments/3.4'),
+	/*'dbparams' => array(
+		'host'  => '127.0.0.1',
+		'user'  => 'travis',
+		'pwd'   => '',
+		'db'    => 'fof_test'
+	)*/
 );
