4a6954402e6f83b3a6347224b4788ac942a3c071
Davide Tampellini <tampe125@gmail.com>
2015-07-15 13:17:01 +0200

    Completed tests for Tags behavior

--- Tests/Model/DataModel/Behaviour/TagsDataprovider.php
+++ Tests/Model/DataModel/Behaviour/TagsDataprovider.php
@@ -2,18 +2,184 @@
 
 class TagsDataprovider
 {
+    public static function getTestOnAfterSave()
+    {
+        $data[] = array(
+            'input' => array(
+                'tags' => null,
+                'input' => array(
+                    'task' => 'publish'
+                ),
+                'mock' => array(
+                    'postProc' => true
+                )
+            ),
+            'check' => array(
+                'case' => 'Task we are not interested into',
+                'exception' => false,
+                'contentType' => 0,
+                'checkContent' => false
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'tags' => null,
+                'input' => array(
+                    'task' => 'apply'
+                ),
+                'mock' => array(
+                    'postProc' => true
+                )
+            ),
+            'check' => array(
+                'case' => 'Apply task',
+                'exception' => false,
+                'contentType' => 2,
+                'checkContent' => true
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'tags' => null,
+                'input' => array(
+                    'task' => 'save'
+                ),
+                'mock' => array(
+                    'postProc' => true
+                )
+            ),
+            'check' => array(
+                'case' => 'Save task',
+                'exception' => false,
+                'contentType' => 2,
+                'checkContent' => true
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'tags' => null,
+                'input' => array(
+                    'task' => 'savenew'
+                ),
+                'mock' => array(
+                    'postProc' => true
+                )
+            ),
+            'check' => array(
+                'case' => 'Savenew task',
+                'exception' => false,
+                'contentType' => 2,
+                'checkContent' => true
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'tags' => array(99),
+                'input' => array(
+                    'task' => 'savenew'
+                ),
+                'mock' => array(
+                    'postProc' => false
+                )
+            ),
+            'check' => array(
+                'case' => "Tags didn't change",
+                'exception' => false,
+                'contentType' => 1,
+                'checkContent' => false
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'tags' => null,
+                'input' => array(
+                    'task' => 'savenew'
+                ),
+                'mock' => array(
+                    'postProc' => false
+                )
+            ),
+            'check' => array(
+                'case' => 'An error occurs in post processing',
+                'exception' => true,
+                'contentType' => 2,
+                'checkContent' => true
+            )
+        );
+
+        return $data;
+    }
+
+    public static function getTestOnAfterDelete()
+    {
+        $data[] = array(
+            'input' => array(
+                'mock' => array(
+                    'delete' => true
+                )
+            ),
+            'check' => array(
+                'exception' => false
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'mock' => array(
+                    'delete' => false
+                )
+            ),
+            'check' => array(
+                'exception' => true
+            )
+        );
+
+        return $data;
+    }
+
     public static function getTestOnAfterBind()
     {
         $data[] = array(
             'input' => array(
                 'load' => 0,
-                'tags' => ''
+                'tags' => '123'
             ),
             'check' => array(
-                'case' => '',
-                'contentType' => '',
-                'addKnown' => '',
-                'result' => ''
+                'case' => 'Tag field already loaded',
+                'contentType' => false,
+                'addKnown' => false,
+                'result' => '123'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'load' => 1,
+                'tags' => null
+            ),
+            'check' => array(
+                'case' => 'Tag field empty - record loaded',
+                'contentType' => true,
+                'addKnown' => true,
+                'result' => 99
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'load' => 0,
+                'tags' => null
+            ),
+            'check' => array(
+                'case' => 'Tag field empty - record not loaded',
+                'contentType' => true,
+                'addKnown' => true,
+                'result' => 99
             )
         );
 
--- Tests/Model/DataModel/Behaviour/TagsTest.php
+++ Tests/Model/DataModel/Behaviour/TagsTest.php
@@ -1,8 +1,12 @@
 <?php
 namespace FOF30\Tests\DataModel;
 
+use FOF30\Input\Input;
 use FOF30\Model\DataModel\Behaviour\Tags;
+use FOF30\Tests\Helpers\ClosureHelper;
 use FOF30\Tests\Helpers\DatabaseTest;
+use FOF30\Tests\Helpers\ReflectionHelper;
+use FOF30\Tests\Helpers\TestContainer;
 use FOF30\Tests\Stubs\Model\DataModelStub;
 
 require_once 'TagsDataprovider.php';
@@ -60,16 +64,105 @@ class TagsTest extends DatabaseTest
 
     /**
      * @group           Behaviour
-     * @group           TagsOnAfterBind
-     * @covers          FOF30\Model\DataModel\Behaviour\Tags::onAfterBind
-     * @dataProvider    TagsDataprovider::getTestOnAfterBind
+     * @group           TagsOnAfterSave
+     * @covers          FOF30\Model\DataModel\Behaviour\Tags::onAfterSave
+     * @dataProvider    TagsDataprovider::getTestOnAfterSave
      */
-    /*public function testOnAfterBind($test, $check)
+    public function testOnAfterSave($test, $check)
     {
+        if($check['exception'])
+        {
+            $this->setExpectedException('Exception');
+        }
+
         $config = array(
             'idFieldName' => 'foftest_foobar_id',
             'tableName'   => '#__foftest_foobars'
         );
+
+        $container = new TestContainer(array(
+            'componentName'	=> 'com_fakeapp',
+            'input'         => new Input($test['input'])
+        ));
+
+        $model = $this->getMock('FOF30\Tests\Stubs\Model\DataModelStub', array('getContentType', 'checkContentType'), array($container, $config));
+        $model->expects($this->exactly($check['contentType']))->method('getContentType')->willReturn('com_foftest.foobars');
+        $model->expects($check['checkContent'] ? $this->once() : $this->never())->method('checkContentType');
+
+        $model->addKnownField('tags');
+        $model->tags = $test['tags'];
+
+        $fakeHelper = new ClosureHelper(array(
+            'getTagIds' => function(){ return 99;},
+            'postStoreProcess' => function() use($test) { return $test['mock']['postProc']; }
+        ));
+
+        $dispatcher = $model->getBehavioursDispatcher();
+        $tags       = new Tags($dispatcher);
+
+        ReflectionHelper::setValue($tags, 'tagsHelper', $fakeHelper);
+
+        $tags->onAfterSave($model);
+
+        // We must have an assertion inside our tests, otherwise phpUnit will complain when running with the strict flag
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * @group           Behaviour
+     * @group           TagsOnAfterDelete
+     * @covers          FOF30\Model\DataModel\Behaviour\Tags::onAfterDelete
+     * @dataProvider    TagsDataprovider::getTestOnAfterDelete
+     */
+    public function testOnAfterDelete($test, $check)
+    {
+        if($check['exception'])
+        {
+            $this->setExpectedException('Exception');
+        }
+
+        $config = array(
+            'idFieldName' => 'foftest_foobar_id',
+            'tableName'   => '#__foftest_foobars'
+        );
+
+        $model = $this->getMock('FOF30\Tests\Stubs\Model\DataModelStub', array('getContentType'), array(static::$container, $config));
+        $model->expects($this->any())->method('getContentType')->willReturn('com_foftest.foobars');
+
+        $fakeHelper = new ClosureHelper(array(
+            'deleteTagData' => function() use($test){ return $test['mock']['delete'];}
+        ));
+
+        $dispatcher = $model->getBehavioursDispatcher();
+        $tags       = new Tags($dispatcher);
+
+        ReflectionHelper::setValue($tags, 'tagsHelper', $fakeHelper);
+
+        $tags->onAfterDelete($model, 1);
+
+        // We must have an assertion inside our tests, otherwise phpUnit will complain when running with the strict flag
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @group           Behaviour
+     * @group           TagsOnAfterBind
+     * @covers          FOF30\Model\DataModel\Behaviour\Tags::onAfterBind
+     * @dataProvider    TagsDataprovider::getTestOnAfterBind
+     */
+    public function testOnAfterBind($test, $check)
+    {
+        $msg = 'Tags::onAfterBind %s - Case: '.$check['case'];
+
+        $config = array(
+            'idFieldName' => 'foftest_foobar_id',
+            'tableName'   => '#__foftest_foobars'
+        );
+
+        // Let's mock the user state getting, since I don't have any valid application
+        $platform = static::$container->platform;
+        $platform::$getUserStateFromRequest = function(){ return null; };
 
         $model = $this->getMock('FOF30\Tests\Stubs\Model\DataModelStub', array('getContentType', 'addKnownField'), array(static::$container, $config));
         $model->expects($check['contentType'] ? $this->once() : $this->never())->method('getContentType')->willReturn('com_foftest.foobars');
@@ -85,14 +178,20 @@ class TagsTest extends DatabaseTest
             $model->tags = $test['tags'];
         }
 
+        $fakeHelper = new ClosureHelper(array(
+            'getTagIds' => function(){ return 99;}
+        ));
+
         $dispatcher = $model->getBehavioursDispatcher();
         $tags       = new Tags($dispatcher);
         $dummy      = array();
 
+        ReflectionHelper::setValue($tags, 'tagsHelper', $fakeHelper);
+
         $tags->onAfterBind($model, $dummy);
 
-        $this->assertEquals($check['result'], $model->tags);
-    }*/
+        $this->assertEquals($check['result'], $model->tags, sprintf($msg, 'Returned the wrong value for the tags'));
+    }
 
     /**
      * @group           Behaviour
