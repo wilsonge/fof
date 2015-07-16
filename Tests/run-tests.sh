561aa9534b98661d25a96dece930d2eae7967458
Davide Tampellini <tampe125@gmail.com>
2015-07-16 15:29:12 +0200

    Removed path from the script (it's already defined in the config file)

--- Tests/run-tests.sh
+++ Tests/run-tests.sh
@@ -4,10 +4,10 @@ if [ "$1" ]
 then
 	if [ $1 == 'native' ]
 	then
-		../vendor/bin/phpunit-randomizer -c ../phpunit.xml ../Tests/
+		../vendor/bin/phpunit-randomizer -c ../phpunit.xml
 	else
-		../vendor/bin/phpunit-randomizer --order seed:$1 -c ../phpunit.xml ../Tests/
+		../vendor/bin/phpunit-randomizer --order seed:$1 -c ../phpunit.xml
 	fi
 else
-	../vendor/bin/phpunit-randomizer --order rand -c ../phpunit.xml ../Tests/
+	../vendor/bin/phpunit-randomizer --order rand -c ../phpunit.xml
 fi
