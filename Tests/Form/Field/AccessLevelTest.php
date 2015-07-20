c0260d9df5eb02647f80412ebe9d15aa9425defe
Davide Tampellini <tampe125@gmail.com>
2015-07-20 16:38:35 +0200

    Added missing tests for AccessLevel field

--- Tests/Form/Field/AccessLevelTest.php
+++ Tests/Form/Field/AccessLevelTest.php
@@ -4,11 +4,88 @@ namespace FOF30\Tests\Form\Field;
 
 use FOF30\Form\Form;
 use FOF30\Tests\Helpers\FOFTestCase;
+use FOF30\Tests\Helpers\ReflectionHelper;
 
 require_once __DIR__.'/AccessLevelDataprovider.php';
 
 class AccessLevelTest extends FOFTestCase
 {
+    /**
+     * @group           Field
+     * @group           AccessLevel__get
+     * @covers          FOF30\Form\Field\AccessLevel::__get
+     * @dataProvider    AccessLevelDataprovider::getTest__get
+     */
+    public function test__get($test, $check)
+    {
+        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getStatic', 'getRepeatable'));
+        $field->expects($this->exactly($check['static']))->method('getStatic');
+        $field->expects($this->exactly($check['repeat']))->method('getRepeatable');
+
+        ReflectionHelper::setValue($field, 'static', $test['static']);
+        ReflectionHelper::setValue($field, 'repeatable', $test['repeat']);
+
+        $property = $test['property'];
+
+        $field->$property;
+    }
+
+    /**
+     * @group           Field
+     * @group           AccessLevelGetStatic
+     * @covers          FOF30\Form\Field\AccessLevel::getStatic
+     * @dataProvider    AccessLevelDataprovider::getTestGetStatic
+     */
+    public function testGetStatic($test, $check)
+    {
+        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getInput', 'getFieldContents'));
+        $field->expects($this->exactly($check['input']))->method('getInput');
+        $field->expects($this->exactly($check['contents']))->method('getFieldContents')->with(array('id' => 'foo'));
+
+        $field->id = 'foo';
+
+        $data  = '<field type="AccessLevel" name="foobar" ';
+
+        if($test['legacy'])
+        {
+            $data .= 'legacy="true"';
+        }
+
+        $data .= ' />';
+        $xml  = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?>'.$data);
+        ReflectionHelper::setValue($field, 'element', $xml);
+
+        $field->getStatic();
+    }
+
+    /**
+     * @group           Field
+     * @group           AccessLevelGetRepeatable
+     * @covers          FOF30\Form\Field\AccessLevel::getRepeatable
+     * @dataProvider    AccessLevelDataprovider::getTestGetRepeatable
+     */
+    public function testGetRepeatable($test, $check)
+    {
+        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getInput', 'getFieldContents'));
+        $field->expects($this->exactly($check['input']))->method('getInput');
+        $field->expects($this->exactly($check['contents']))->method('getFieldContents')->with(array('class' => 'foo'));
+
+        $field->id = 'foo';
+
+        $data  = '<field type="AccessLevel" name="foobar" ';
+
+        if($test['legacy'])
+        {
+            $data .= 'legacy="true"';
+        }
+
+        $data .= ' />';
+        $xml  = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?>'.$data);
+        ReflectionHelper::setValue($field, 'element', $xml);
+
+        $field->getRepeatable();
+    }
+
     /**
      * @group           Field
      * @group           AccessLevelGetFieldContents
