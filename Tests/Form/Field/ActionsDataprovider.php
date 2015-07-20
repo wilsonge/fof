80525813c5780be15e16df0f58c4e501682c4bbe
Davide Tampellini <tampe125@gmail.com>
2015-07-20 17:30:02 +0200

    Added tests for Actions field (WIP)

---
+++ Tests/Form/Field/ActionsDataprovider.php
@@ -0,0 +1,84 @@
+<?php
+
+class ActionsDataprovider
+{
+    public static function getTest__get()
+    {
+        $data[] = array(
+            'input' => array(
+                'property' => 'static',
+                'static'   => null,
+                'repeat'   => null
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the static method, not cached',
+                'static' => 1,
+                'repeat' => 0
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'property' => 'static',
+                'static'   => 'cached',
+                'repeat'   => null
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the static method, cached',
+                'static' => 0,
+                'repeat' => 0
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'property' => 'repeatable',
+                'static'   => null,
+                'repeat'   => null
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the repeatable method, not cached',
+                'static' => 0,
+                'repeat' => 1
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'property' => 'repeatable',
+                'static'   => null,
+                'repeat'   => 'cached'
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the repeatable method, cached',
+                'static' => 0,
+                'repeat' => 0
+            )
+        );
+
+        return $data;
+    }
+
+    public static function getTestGetRepeatable()
+    {
+        $data[] = array(
+            'input' => array(
+                'id'          => 'foftest_foobar_id',
+                'table'       => '#__foftest_foobars',
+                'enabled'     => 1,
+                'published'   => 1,
+                'unpublished' => 1,
+                'archived'    => 1,
+                'trash'       => 1,
+                'all'         => 1,
+            ),
+            'check' => array(
+                'case'   => '',
+                'result' => '',
+                'publishField' => 1
+            )
+        );
+
+        return $data;
+    }
+}
---
+++ Tests/Form/Field/ActionsTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace FOF30\Tests\Form\Field;
+
+use FOF30\Form\Field\Actions;
+use FOF30\Tests\Helpers\ClosureHelper;
+use FOF30\Tests\Helpers\FOFTestCase;
+use FOF30\Tests\Helpers\ReflectionHelper;
+use FOF30\Tests\Stubs\Model\DataModelStub;
+
+require_once __DIR__.'/ActionsDataprovider.php';
+
+/**
+ * @covers  FOF30\Form\Field\Actions::<private>
+ * @covers  FOF30\Form\Field\Actions::<protected>
+ */
+class ActionsTest extends FOFTestCase
+{
+    /**
+     * @group           Field
+     * @group           Actions__get
+     * @covers          FOF30\Form\Field\Actions::__get
+     * @dataProvider    ActionsDataprovider::getTest__get
+     */
+    public function test__get($test, $check)
+    {
+        $field = $this->getMock('FOF30\Form\Field\Actions', array('getStatic', 'getRepeatable'));
+        $field->expects($this->exactly($check['static']))->method('getStatic');
+        $field->expects($this->exactly($check['repeat']))->method('getRepeatable');
+
+        ReflectionHelper::setValue($field, 'static', $test['static']);
+        ReflectionHelper::setValue($field, 'repeatable', $test['repeat']);
+
+        $property = $test['property'];
+
+        $field->$property;
+    }
+
+    /**
+     * @group           Field
+     * @group           ActionsGetStatic
+     * @covers          FOF30\Form\Field\Actions::getStatic
+     */
+    public function testGetStatic()
+    {
+        $this->setExpectedException('FOF30\Form\Exception\GetStaticNotAllowed');
+
+        $field = new Actions();
+
+        $field->getStatic();
+    }
+
+    /**
+     * @group           Field
+     * @group           ActionsGetRepeatable
+     * @covers          FOF30\Form\Field\Actions::getRepeatable
+     * @dataProvider    ActionsDataprovider::getTestGetRepeatable
+     */
+    /*public function testGetRepeatable($test, $check)
+    {
+        $msg = 'Actions::getRepeatable %s - Case: '.$check['case'];
+
+        $config = array(
+            'idFieldName' => $test['id'],
+            'tableName'   => $test['table']
+        );
+
+        $model = new DataModelStub(static::$container, $config);
+        $model->setFieldValue('enabled', $test['enabled']);
+
+        $fakeField = new ClosureHelper(array(
+            'getRepeatable' => function(){
+                return '__FAKE_PUBLISH__';
+            }
+        ));
+
+        $field = $this->getMock('FOF30\Form\Field\Actions', array('getPublishedField'));
+        $field->expects($this->exactly($check['publishField']))->method('getPublishedField')->willReturn($fakeField);
+
+        $data  = '<field type="Actions"';
+        $data .= ' show_published="'.($test['published'] ? 1 : 0).'"';
+        $data .= ' show_unpublished="'.($test['unpublished'] ? 1 : 0).'"';
+        $data .= ' show_archived="'.($test['archived'] ? 1 : 0).'"';
+        $data .= ' show_trash="'.($test['trash'] ? 1 : 0).'"';
+        $data .= ' show_all="'.($test['all'] ? 1 : 0).'"';
+
+        $data .= ' />';
+
+        $xml  = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?>'.$data);
+
+        ReflectionHelper::setValue($field, 'element', $xml);
+        ReflectionHelper::setValue($field, 'item', $model);
+
+        $html = $field->getRepeatable();
+
+        $this->assertEquals($check['result'], $html, sprintf($msg, 'Returned the wrong result'));
+    }*/
+
+    /**
+     * @group           Field
+     * @group           ActionsGetRepeatable
+     * @covers          FOF30\Form\Field\Actions::getRepeatable
+     */
+    public function testGetRepeatableException()
+    {
+        $this->setExpectedException('FOF30\Form\Exception\DataModelRequired');
+
+        $field = new Actions();
+
+        $field->getRepeatable();
+    }
+}
