e4c564f97022f9f58c73592db794d5d3cfa695cf
Davide Tampellini <tampe125@gmail.com>
2015-07-31 15:33:20 +0200

    Completed tests for Model field

--- Tests/Form/Field/ModelDataprovider.php
+++ Tests/Form/Field/ModelDataprovider.php
@@ -65,10 +65,138 @@ class ModelDataprovider
     {
         $data[] = array(
             'input' => array(
-
+                'item'       => false,
+                'attribs'    => array(),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class'
+                )
             ),
             'check' => array(
+                'case'   => 'No value selected, no empty replacement',
+                'result' => '<span class="foo-class"></span>'
+            )
+        );
 
+        $data[] = array(
+            'input' => array(
+                'item'       => false,
+                'attribs'    => array(
+                    'empty_replacement' => 'Placeholder'
+                ),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class'
+                )
+            ),
+            'check' => array(
+                'case'   => 'No value selected, with empty replacement',
+                'result' => '<span class="foo-class">Placeholder</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'item'       => false,
+                'attribs'    => array(
+                    'empty_replacement' => 'JYES'
+                ),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class'
+                )
+            ),
+            'check' => array(
+                'case'   => 'No value selected, with empty replacement (translated)',
+                'result' => '<span class="foo-class">Yes</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'item'       => false,
+                'attribs'    => array(
+                    'empty_replacement' => '2',
+                    'format' => '%F'
+                ),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class'
+                )
+            ),
+            'check' => array(
+                'case'   => 'No value selected, with empty replacement (formatted)',
+                'result' => '<span class="foo-class">'.sprintf('%F', 2).'</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'item'       => false,
+                'attribs'    => array(
+                    'empty_replacement' => '2',
+                ),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class',
+                    'value' => 'dummy'
+                )
+            ),
+            'check' => array(
+                'case'   => 'With value selected',
+                'result' => '<span class="foo-class">Dummy</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'item'       => false,
+                'attribs'    => array(
+                    'url' => 'somelink'
+                ),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class',
+                    'value' => 'dummy'
+                )
+            ),
+            'check' => array(
+                'case'   => 'With link but no datamodel',
+                'result' => '<span class="foo-class">Dummy</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'item'       => true,
+                'attribs'    => array(),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class',
+                    'value' => 'dummy'
+                )
+            ),
+            'check' => array(
+                'case'   => 'With datamodel but no link',
+                'result' => '<span class="foo-class">Dummy</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'item'       => true,
+                'attribs'    => array(
+                    'url' => 'somelink'
+                ),
+                'properties' => array(
+                    'id' => 'foo-id',
+                    'class' => 'foo-class',
+                    'value' => 'dummy'
+                )
+            ),
+            'check' => array(
+                'case'   => 'With datamodel and link',
+                'result' => '<span class="foo-class"><a href="__PARSED__">Dummy</a></span>'
             )
         );
 
@@ -110,6 +238,285 @@ class ModelDataprovider
             'check' => array(
                 'case' => 'Record assigned and loaded, field not existing',
                 'result' => '__WRONG:[ITEM:WRONG]__'
+            )
+        );
+
+        return $data;
+    }
+
+    public static function getTestGetOptions()
+    {
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title'
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Simple fetching from the model',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => '1', 'text' => 'First', 'disable' => false),
+                    (object) array('value' => '2', 'text' => 'Second', 'disable' => false),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(
+                    'Foobar#$#foobars' => array(
+                        (object) array('value' => '100', 'title' => 'Cached'),
+                    )
+                ),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title'
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Cache hit',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => '100', 'title' => 'Cached'),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => true,
+                'cache'    => array(
+                    'Foobar#$#foobars' => array(
+                        (object) array('value' => '100', 'title' => 'Cached'),
+                    )
+                ),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title'
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Cache loaded, but we force refresh',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => '1', 'text' => 'First', 'disable' => false),
+                    (object) array('value' => '2', 'text' => 'Second', 'disable' => false),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title',
+                    'apply_access' => 1,
+                    'with' => 'foo,bar'
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Using "with" and apply access filtering',
+                'applyAccess' => 1,
+                'with'        => 1,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => '1', 'text' => 'First', 'disable' => false),
+                    (object) array('value' => '2', 'text' => 'Second', 'disable' => false),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title'
+                ),
+                'children' => array(
+                    'state'   => array(
+                        'foo'   => 'bar',
+                        'dummy' => 'dummy'
+                    ),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Setting state variables to the model',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(
+                    'foo'   => 'bar',
+                    'dummy' => 'dummy'
+                ),
+                'result'      => array (
+                    (object) array('value' => '1', 'text' => 'First', 'disable' => false),
+                    (object) array('value' => '2', 'text' => 'Second', 'disable' => false),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title',
+                    'key_field' => 'key',
+                    'value_field' => 'value',
+                    'none' => 'None placeholder',
+                    'translate' => true
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('key' => '1', 'value' => 'JYES'),
+                        (object) array('key' => '2', 'value' => 'JNO'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Using field XML attributes',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => '', 'text' => 'None placeholder', 'disable' => false),
+                    (object) array('value' => '1', 'text' => 'Yes', 'disable' => false),
+                    (object) array('value' => '2', 'text' => 'No', 'disable' => false),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title'
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array(
+                        'foo' => 'Bar',
+                        'dummy' => 'Dummy'
+                    )
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Merging with XML options',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => 'foo', 'text' => 'Bar', 'disable' => false, 'class' => false, 'onclick' => false),
+                    (object) array('value' => 'dummy', 'text' => 'Dummy', 'disable' => false, 'class' => false, 'onclick' => false),
+                    (object) array('value' => '1', 'text' => 'First', 'disable' => false),
+                    (object) array('value' => '2', 'text' => 'Second', 'disable' => false),
+                )
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'force'    => false,
+                'cache'    => array(),
+                'attribs'  => array(
+                    'model' => 'foobars',
+                    'name'  => 'title',
+                    'parse_value' => 1
+                ),
+                'children' => array(
+                    'state'   => array(),
+                    'options' => array()
+                ),
+                'mock'     => array(
+                    'get'     => array(
+                        (object) array('value' => '1', 'title' => 'First'),
+                        (object) array('value' => '2', 'title' => 'Second'),
+                    )
+                )
+            ),
+            'check' => array(
+                'case'        => 'Parse values',
+                'applyAccess' => 0,
+                'with'        => 0,
+                'setState'    => array(),
+                'result'      => array (
+                    (object) array('value' => '1', 'text' => '__PARSED__', 'disable' => false),
+                    (object) array('value' => '2', 'text' => '__PARSED__', 'disable' => false),
+                )
             )
         );
 
--- Tests/Form/Field/ModelTest.php
+++ Tests/Form/Field/ModelTest.php
@@ -7,6 +7,7 @@ use FOF30\Form\Form;
 use FOF30\Tests\Helpers\ClosureHelper;
 use FOF30\Tests\Helpers\DatabaseTest;
 use FOF30\Tests\Helpers\ReflectionHelper;
+use FOF30\Tests\Helpers\TestContainer;
 use FOF30\Tests\Stubs\Model\DataModelStub;
 
 require_once __DIR__.'/ModelDataprovider.php';
@@ -39,20 +40,151 @@ class ModelTest extends DatabaseTest
 
     /**
      * @group           ModelField
+     * @group           ModelGetStatic
+     * @covers          FOF30\Form\Field\Model::getStatic
+     */
+    public function testGetStatic()
+    {
+        $field = $this->getMock('FOF30\Form\Field\Model', array('getOptions'));
+        $field->method('getOptions')->willReturn(array(
+            array('value' => 'foo', 'text' => 'Foobar'),
+            array('value' => 'dummy', 'text' => 'Dummy'),
+        ));
+
+        $field->id    = 'foo';
+        $field->class = 'foo-class';
+        $field->value = 'dummy';
+
+        $result = $field->getStatic();
+
+        $this->assertEquals('<span id="foo" class="foo-class">Dummy</span>', $result);
+    }
+
+    /**
+     * @group           ModelField
      * @group           ModelGetRepeatable
      * @covers          FOF30\Form\Field\Model::getRepeatable
      * @dataProvider    FOF30\Tests\Form\Field\ModelDataprovider::getTestGetRepeatable
      */
-    /*public function testGetRepeatable($test, $check)
+    public function testGetRepeatable($test, $check)
     {
         $msg = 'Model::getRepeatable %s - Case: '.$check['case'];
 
-        $field = new Model();
+        $field = $this->getMock('FOF30\Form\Field\Model', array('parseFieldTags', 'getOptions'));
+        $field->method('parseFieldTags')->willReturn('__PARSED__');
+        $field->method('getOptions')->willReturn(array(
+            array('value' => 'foo', 'text' => 'Foobar'),
+            array('value' => 'dummy', 'text' => 'Dummy'),
+        ));
+
+        $data = '<field type="Model" ';
+
+        foreach($test['attribs'] as $key => $value)
+        {
+            $data .= $key.'="'.$value.'" ';
+        }
+
+        $data .= '/>';
+
+        $xml = simplexml_load_string($data);
+        ReflectionHelper::setValue($field, 'element', $xml);
+
+        foreach ($test['properties'] as $key => $value)
+        {
+            ReflectionHelper::setValue($field, $key, $value);
+        }
+
+        if($test['item'])
+        {
+            $config = array(
+                'tableName' => '#__foftest_foobars',
+                'idFieldName' => 'foftest_foobar_id'
+            );
+
+            $item = new DataModelStub(static::$container, $config);
+            ReflectionHelper::setValue($field, 'item', $item);
+        }
 
         $result = $field->getRepeatable();
 
         $this->assertEquals($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
-    }*/
+    }
+
+    /**
+     * @group           ModelField
+     * @group           ModelGetOptions
+     * @covers          FOF30\Form\Field\Model::getOptions
+     * @dataProvider    FOF30\Tests\Form\Field\ModelDataprovider::getTestGetOptions
+     */
+    public function testGetOptions($test, $check)
+    {
+        $msg = 'Model::getOptions %s - Case: '.$check['case'];
+
+        $callstack = array();
+
+        $config = array(
+            'tableName' => '#__foftest_foobars',
+            'idFieldName' => 'foftest_foobar_id'
+        );
+
+        $methods = array('applyAccessFiltering', 'with', 'setState', 'get');
+        $model   = $this->getMock('FOF30\Tests\Stubs\Model\DataModelStub', $methods, array(static::$container, $config));
+        $model->expects($this->exactly($check['applyAccess']))->method('applyAccessFiltering');
+        $model->expects($this->exactly($check['with']))->method('with');
+        $model->method('get')->willReturn($test['mock']['get']);
+        $model->method('setState')->willReturnCallback(function($key, $value) use(&$callstack){
+            $callstack[$key] = $value;
+        });
+
+        // Let's create our mocked factory that will return our mocked model
+        $container = new TestContainer(array(
+            'factory' => function() use ($model){
+                return new ClosureHelper(array(
+                    'model' => function() use($model){ return $model; }
+                ));
+            }
+        ));
+
+        $field = $this->getMock('FOF30\Form\Field\Model', array('parseFieldTags'));
+        $field->method('parseFieldTags')->willReturn('__PARSED__');
+
+        $form = new Form($container, 'Foobar');
+        $field->setForm($form);
+
+        ReflectionHelper::setValue($field, 'loadedOptions', $test['cache']);
+
+        $data = '<field type="Model" ';
+
+        // Let's add field attributes
+        foreach($test['attribs'] as $key => $value)
+        {
+            $data .= $key.'="'.$value.'" ';
+        }
+
+        $data .= '>';
+
+        // Let's add state children elements
+        foreach($test['children']['state'] as $key => $value)
+        {
+            $data .= '<state key="'.$key.'">'.$value.'</state>';
+        }
+
+        // Let's add state children elements
+        foreach($test['children']['options'] as $value => $text)
+        {
+            $data .= '<option value="'.$value.'">'.$text.'</option>';
+        }
+
+        $data .= '</field>';
+
+        $xml = simplexml_load_string($data);
+        ReflectionHelper::setValue($field, 'element', $xml);
+
+        $result = ReflectionHelper::invoke($field, 'getOptions', $test['force']);
+
+        $this->assertEquals($check['result'], $result, sprintf($msg, 'Returned the wrong result'));
+        $this->assertEquals($check['setState'], $callstack, sprintf($msg, 'Failed to correctly set the state of the model'));
+    }
 
     /**
      * @group           ModelField
