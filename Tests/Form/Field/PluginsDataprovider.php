f12c82d48e384daf4da99b5a9b637033f266cc01
Davide Tampellini <tampe125@gmail.com>
2015-08-03 14:06:52 +0200

    Added tests for Plugin field

---
+++ Tests/Form/Field/PluginsDataprovider.php
@@ -0,0 +1,157 @@
+<?php
+
+class PluginsDataprovider
+{
+    public static function getTest__get()
+    {
+        $data[] = array(
+            'input' => array(
+                'property' => 'static',
+                'static'   => null,
+                'repeat'   => null
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the static method, not cached',
+                'static' => 1,
+                'repeat' => 0
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'property' => 'static',
+                'static'   => 'cached',
+                'repeat'   => null
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the static method, cached',
+                'static' => 0,
+                'repeat' => 0
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'property' => 'repeatable',
+                'static'   => null,
+                'repeat'   => null
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the repeatable method, not cached',
+                'static' => 0,
+                'repeat' => 1
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'property' => 'repeatable',
+                'static'   => null,
+                'repeat'   => 'cached'
+            ),
+            'check' => array(
+                'case'   => 'Requesting for the repeatable method, cached',
+                'static' => 0,
+                'repeat' => 0
+            )
+        );
+
+        return $data;
+    }
+
+    public static function getTestGetStatic()
+    {
+        $data[] = array(
+            'input' => array(
+                'legacy' => true
+            ),
+            'check' => array(
+                'case'     => 'Using the legacy attribute',
+                'input'    => 1,
+                'contents' => 0
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'legacy' => false
+            ),
+            'check' => array(
+                'case'     => 'Without using the legacy attribute',
+                'input'    => 0,
+                'contents' => 1
+            )
+        );
+
+        return $data;
+    }
+
+    public static function getTestGetRepeatable()
+    {
+        $data[] = array(
+            'input' => array(
+                'legacy' => true
+            ),
+            'check' => array(
+                'case'     => 'Using the legacy attribute',
+                'input'    => 1,
+                'contents' => 0
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'legacy' => false
+            ),
+            'check' => array(
+                'case'     => 'Without using the legacy attribute',
+                'input'    => 0,
+                'contents' => 1
+            )
+        );
+
+        return $data;
+    }
+
+    public static function getTestGetFieldContents()
+    {
+        $data[] = array(
+            'input' => array(
+                'mock' => array(
+                    'options' => array(
+                        'plg_system_sef' => 'sef',
+                        'plg_system_admintools' => 'AdminTools'
+                    )
+                ),
+                'value'   => 'plg_system_admintools',
+                'options' => array()
+            ),
+            'check' => array(
+                'case'   => 'No field options',
+                'result' => '<span class="">AdminTools</span>'
+            )
+        );
+
+        $data[] = array(
+            'input' => array(
+                'mock' => array(
+                    'options' => array(
+                        'plg_system_sef' => 'sef',
+                        'plg_system_admintools' => 'AdminTools'
+                    )
+                ),
+                'value'   => '',
+                'options' => array(
+                    'id' => 'foobar',
+                    'class' => 'foo-class'
+                )
+            ),
+            'check' => array(
+                'case'   => 'With field options, no value',
+                'result' => '<span id="foobar" class=" foo-class">sef</span>'
+            )
+        );
+
+        return $data;
+    }
+}
--- Tests/Form/Field/PluginsTest.php
+++ Tests/Form/Field/PluginsTest.php
@@ -6,23 +6,23 @@ use FOF30\Form\Form;
 use FOF30\Tests\Helpers\FOFTestCase;
 use FOF30\Tests\Helpers\ReflectionHelper;
 
-require_once __DIR__.'/AccessLevelDataprovider.php';
+require_once __DIR__.'/PluginsDataprovider.php';
 
 /**
- * @covers  FOF30\Form\Field\AccessLevel::<private>
- * @covers  FOF30\Form\Field\AccessLevel::<protected>
+ * @covers  FOF30\Form\Field\Plugins::<private>
+ * @covers  FOF30\Form\Field\Plugins::<protected>
  */
-class AccessLevelTest extends FOFTestCase
+class PluginsTest extends FOFTestCase
 {
     /**
-     * @group           AccessLevel
-     * @group           AccessLevel__get
-     * @covers          FOF30\Form\Field\AccessLevel::__get
-     * @dataProvider    AccessLevelDataprovider::getTest__get
+     * @group           Plugins
+     * @group           Plugins__get
+     * @covers          FOF30\Form\Field\Plugins::__get
+     * @dataProvider    PluginsDataprovider::getTest__get
      */
     public function test__get($test, $check)
     {
-        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getStatic', 'getRepeatable'));
+        $field = $this->getMock('FOF30\Form\Field\Plugins', array('getStatic', 'getRepeatable'));
         $field->expects($this->exactly($check['static']))->method('getStatic');
         $field->expects($this->exactly($check['repeat']))->method('getRepeatable');
 
@@ -35,20 +35,20 @@ class AccessLevelTest extends FOFTestCase
     }
 
     /**
-     * @group           AccessLevel
-     * @group           AccessLevelGetStatic
-     * @covers          FOF30\Form\Field\AccessLevel::getStatic
-     * @dataProvider    AccessLevelDataprovider::getTestGetStatic
+     * @group           Plugins
+     * @group           PluginsGetStatic
+     * @covers          FOF30\Form\Field\Plugins::getStatic
+     * @dataProvider    PluginsDataprovider::getTestGetStatic
      */
     public function testGetStatic($test, $check)
     {
-        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getInput', 'getFieldContents'));
+        $field = $this->getMock('FOF30\Form\Field\Plugins', array('getInput', 'getFieldContents'));
         $field->expects($this->exactly($check['input']))->method('getInput');
         $field->expects($this->exactly($check['contents']))->method('getFieldContents')->with(array('id' => 'foo'));
 
         $field->id = 'foo';
 
-        $data  = '<field type="AccessLevel" name="foobar" ';
+        $data  = '<field type="Plugins" name="foobar" ';
 
         if($test['legacy'])
         {
@@ -63,20 +63,20 @@ class AccessLevelTest extends FOFTestCase
     }
 
     /**
-     * @group           AccessLevel
-     * @group           AccessLevelGetRepeatable
-     * @covers          FOF30\Form\Field\AccessLevel::getRepeatable
-     * @dataProvider    AccessLevelDataprovider::getTestGetRepeatable
+     * @group           Plugins
+     * @group           PluginsGetRepeatable
+     * @covers          FOF30\Form\Field\Plugins::getRepeatable
+     * @dataProvider    PluginsDataprovider::getTestGetRepeatable
      */
     public function testGetRepeatable($test, $check)
     {
-        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getInput', 'getFieldContents'));
+        $field = $this->getMock('FOF30\Form\Field\Plugins', array('getInput', 'getFieldContents'));
         $field->expects($this->exactly($check['input']))->method('getInput');
         $field->expects($this->exactly($check['contents']))->method('getFieldContents')->with(array('class' => 'foo'));
 
         $field->id = 'foo';
 
-        $data  = '<field type="AccessLevel" name="foobar" ';
+        $data  = '<field type="Plugins" name="foobar" ';
 
         if($test['legacy'])
         {
@@ -91,20 +91,17 @@ class AccessLevelTest extends FOFTestCase
     }
 
     /**
-     * @group           AccessLevel
-     * @group           AccessLevelGetFieldContents
-     * @covers          FOF30\Form\Field\AccessLevel::getFieldContents
-     * @dataProvider    AccessLevelDataprovider::getTestGetFieldContents
+     * @group           Plugins
+     * @group           PluginsGetFieldContents
+     * @covers          FOF30\Form\Field\Plugins::getFieldContents
+     * @dataProvider    PluginsDataprovider::getTestGetFieldContents
      */
     public function testGetFieldContents($test, $check)
     {
-        $msg = 'AccessLevel::getFieldContents %s - Case: '.$check['case'];
+        $msg = 'Plugins::getFieldContents %s - Case: '.$check['case'];
 
-        $field = $this->getMock('FOF30\Form\Field\AccessLevel', array('getOptions'));
+        $field = $this->getMock('FOF30\Form\Field\Plugins', array('getOptions'));
         $field->method('getOptions')->willReturn($test['mock']['options']);
-
-        $form = new Form(static::$container, 'Foobar');
-        $field->setForm($form);
 
         // Registered access level
         $field->value = $test['value'];
