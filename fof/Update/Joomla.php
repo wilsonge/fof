00505da10c829aefc22cc60384ca57224dde92c2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-11-18 11:05:03 +0200

    # [LOW] Joomla! STS/LTS status per version has changed. Updating FOF30\Update\Joomla to reflect that.

--- fof/Update/Joomla.php
+++ fof/Update/Joomla.php
@@ -107,28 +107,19 @@ class Joomla extends Extension
 				$lts_minimum = '2.5';
 				break;
 
+			case '2.5':
+				$ret['lts'] = true;
+				$sts_minimum = false;
+				$lts_minimum = '2.5';
+				break;
+
 			default:
 				$majorVersion = (int)substr($jVersion, 0, 1);
 				$minorVersion = (int)substr($jVersion, 2, 1);
 
-				if ($minorVersion == 5)
-				{
-					$ret['lts'] = true;
-					// This is an LTS release, it can be superseded by .0 through .4 STS releases on the next branch...
-					$sts_minimum = ($majorVersion + 1) . '.0';
-					$sts_maximum = ($majorVersion + 1) . '.4.9999';
-					// ...or a .5 LTS on the next branch
-					$lts_minimum = ($majorVersion + 1) . '.5';
-				}
-				else
-				{
-					$ret['lts'] = false;
-					// This is an STS release, it can be superseded by a .1/.2/.3/.4 STS release on the same branch...
-					$sts_minimum = $majorVersion . '.1';
-					$sts_maximum = $majorVersion . '.4.9999';
-					// ...or a .5 LTS on the same branch
-					$lts_minimum = $majorVersion . '.5';
-				}
+				$ret['lts'] = true;
+				$sts_minimum = false;
+				$lts_minimum = $majorVersion . '.0';
 				break;
 		}
 
